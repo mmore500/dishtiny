f4b0e4ae0d151f64b91c1ec298bd4176a6b20e1e
Matthew Andres Moreno <m.more500@gmail.com>
2019-02-19 11:32:48 -0500

    Remove duplicate instruction

--- source/LibraryInstruction.h
+++ source/LibraryInstruction.h
@@ -431,34 +431,6 @@ public:
     );
 
     il.AddInst(
-      "QueryIsChild",
-      [is_live](hardware_t & hw, const inst_t & inst){
-        state_t & state = hw.GetCurState();
-
-        FrameHardware &fh = *hw.GetTrait(0);
-
-        const size_t dir = CalcDir(fh,state.GetLocal(inst.args[0]));
-        const size_t neigh = fh.Cell().GetNeigh(dir);
-
-        bool match = true;
-
-        if(is_live(neigh)) {
-          Manager &man = fh.Cell().Man();
-          size_t pos = fh.Cell().GetPos();
-          match &= man.Family(pos).HasChildPos(neigh);
-          match &= man.Family(neigh).IsParentPos(pos);
-        } else {
-          match = false;
-        }
-
-        state.SetLocal(inst.args[1], match);
-
-      },
-      2,
-      "TODO"
-    );
-
-    il.AddInst(
       "QueryIsParent",
       [is_live](hardware_t & hw, const inst_t & inst){
         state_t & state = hw.GetCurState();
