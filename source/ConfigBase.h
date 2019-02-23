041fa2e63e9320756bc0b7bc53ab78d6f9649214
Matthew Andres Moreno <m.more500@gmail.com>
2019-02-23 16:17:23 -0500

    Increase mutation rate and maximum func len

--- source/ConfigBase.h
+++ source/ConfigBase.h
@@ -22,7 +22,7 @@ EMP_BUILD_CONFIG(
   GROUP(PROGRAM_GROUP, "SignalGP program Settings"),
   VALUE(PROGRAM_MAX_FUN_CNT, size_t, 8, "Used for generating SGP programs. How many functions do we generate?"),
   VALUE(PROGRAM_MIN_FUN_CNT, size_t, 1, "Used for generating SGP programs. How many functions do we generate?"),
-  VALUE(PROGRAM_MAX_FUN_LEN, size_t, 8, ".."),
+  VALUE(PROGRAM_MAX_FUN_LEN, size_t, 24, ".."),
   VALUE(PROGRAM_MIN_FUN_LEN, size_t, 1, ".."),
   VALUE(PROGRAM_MAX_TOTAL_LEN, size_t, 256, "Maximum length of SGP programs."),
 
@@ -36,7 +36,7 @@ EMP_BUILD_CONFIG(
   VALUE(INBOX_CAPACITY, size_t, 128, "Capacity of a cell's inbox."),
 
   GROUP(MUTATION_GROUP, "SignalGP Mutation Settings"),
-  VALUE(MUTATION_RATE, double, 0.02, "What percentage of reproductions should be subject to mutations at all?"),
+  VALUE(MUTATION_RATE, double, 0.2, "What percentage of reproductions should be subject to mutations at all?"),
   VALUE(PROGRAM_MIN_ARG_VAL, int, 0, "Maximum argument value for instructions."),
   VALUE(PROGRAM_MAX_ARG_VAL, int, 16, "Maximum argument value for instructions."),
   VALUE(TAG_BIT_FLIP__PER_BIT, double, 0.005, "Per-bit mutation rate of tag bit flips."),
