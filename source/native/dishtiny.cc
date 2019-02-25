0f6edf0d68b9f4c6ade2ab6e90dec825de9d824f
Matthew Andres Moreno <m.more500@gmail.com>
2019-02-25 10:26:44 -0500

    Print git version at start of native run

--- source/native/dishtiny.cc
+++ source/native/dishtiny.cc
@@ -20,6 +20,8 @@
 int main(int argc, char* argv[])
 {
 
+  std::cout << "GIT VERSION " << STRINGIFY(GIT_VERSION_) << std::endl;
+
   // Read configs.
   std::string config_fname = "configs.cfg";
   Config cfg(config_fname, emp::cl::ArgManager(argc, argv));
