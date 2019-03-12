c47117dbaca2ee5597bca88c98ea04680d11a758
Matthew Andres Moreno <m.more500@gmail.com>
2019-03-12 12:36:26 -0400

    Add between-treatment analysis scripts

---
+++ script/CalcApoptosis.py
@@ -0,0 +1,45 @@
+# usage:
+# first_update last_update h5_filenames
+
+import numpy as np
+import h5py
+import sys
+from tqdm import tqdm
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+first_update = int(sys.argv[1])
+last_update = int(sys.argv[2])
+filenames = sys.argv[3:]
+
+repThresh = 3
+
+def FracApoptosis(file):
+    return np.mean([
+        1 if apop[idx] else 0 # 1 and 2 are apoptosis types
+        for apop in [
+            np.array(
+                file['Apoptosis']['upd_'+str(upd)]
+            ).flatten()
+            for upd in range(first_update, last_update)
+        ]
+        for idx in range(file['Index']['own'].size)
+    ])
+
+def ExtractTreat(filename):
+    return next(str for str in filename.split('+') if "treat=" in str)
+
+data = pd.DataFrame.from_dict([
+    {
+        'Treat' : treat,
+        'FracApoptosis' : FracApoptosis(file),
+    }
+    for treat, file in (
+        (ExtractTreat(filename), h5py.File(filename, 'r')) for filename in filenames
+    )
+])
+
+print("num files:" , len(filenames))
+sns.barplot(x="Treat", y="FracApoptosis", data=data)
+plt.show()
---
+++ script/CalcStockpile.py
@@ -0,0 +1,61 @@
+# usage:
+# first_update last_update h5_filenames
+
+import numpy as np
+import h5py
+import sys
+from tqdm import tqdm
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+first_update = int(sys.argv[1])
+last_update = int(sys.argv[2])
+filenames = sys.argv[3:]
+
+repThresh = 3
+
+def FracOverRepThresh(file):
+    return np.mean([
+        1 if stock[idx] >= repThresh else 0
+        for stock in [
+            np.array(
+                file['Stockpile']['upd_'+str(upd)]
+            ).flatten()
+            for upd in range(first_update, last_update)
+        ]
+        for idx in range(file['Index']['own'].size)
+    ])
+
+def MeanOverRepThreshAmt(file):
+    return np.mean([
+        stock[idx]
+        for stock in [
+            np.array(
+                file['Stockpile']['upd_'+str(upd)]
+            ).flatten()
+            for upd in range(first_update, last_update)
+        ]
+        for idx in range(file['Index']['own'].size)
+        if stock[idx] >= repThresh
+    ])
+
+def ExtractTreat(filename):
+    return next(str for str in filename.split('+') if "treat=" in str)
+
+data = pd.DataFrame.from_dict([
+    {
+        'Treat' : treat,
+        'FracOverRepThresh' : FracOverRepThresh(file),
+        'MeanOverRepThreshAmt' : MeanOverRepThreshAmt(file)
+    }
+    for treat, file in (
+        (ExtractTreat(filename), h5py.File(filename, 'r')) for filename in filenames
+    )
+])
+
+print("num files:" , len(filenames))
+sns.barplot(x="Treat", y="FracOverRepThresh", data=data)
+plt.show()
+sns.barplot(x="Treat", y="MeanOverRepThreshAmt", data=data)
+plt.show()
