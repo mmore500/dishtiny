a47d5a45acb47684f218266235cb49ddd358e984
Matthew Andres Moreno <m.more500@gmail.com>
2019-03-15 15:39:01 -0400

    Take means of each pair over all updates

--- script/EndowmentPrep.py
+++ script/EndowmentPrep.py
@@ -32,48 +32,52 @@ nlev = file.attrs['NLEV'][0]
 ntile = file['Index']['own'].size
 
 propagule_parent = [
-    rc[idx]
+    np.mean([
+        rc[idx]
+        for rc, pc, ch, dir in [
+            (np.array(
+                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['PrevChan']['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['Index'][dir_key]
+            ).flatten())
+            for upd in range(first_update, last_update)
+        ]
+        if (ch[dir[idx]] != ch[idx] and pc[idx] == ch[dir[idx]])
+    ])
     for dir_key in file['ResourceContributed']
-    for rc, pc, ch, dir in [
-        (np.array(
-            file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['PrevChan']['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['Index'][dir_key]
-        ).flatten())
-        for upd in range(first_update, last_update)
-    ]
     for idx in range(file['Index']['own'].size)
-    if (ch[dir[idx]] != ch[idx] and pc[idx] == ch[dir[idx]])
 ]
 
 
 not_propagule_parent = [
-    rc[idx]
+    np.mean([
+        rc[idx]
+        for rc, pc, ch, dir in [
+            (np.array(
+                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['PrevChan']['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
+            ).flatten(),
+            np.array(
+                file['Index'][dir_key]
+            ).flatten())
+            for upd in range(first_update, last_update)
+        ]
+        if (ch[dir[idx]] != ch[idx] and pc[idx] != ch[dir[idx]])
+    ])
     for dir_key in file['ResourceContributed']
-    for rc, pc, ch, dir in [
-        (np.array(
-            file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['PrevChan']['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-        ).flatten(),
-        np.array(
-            file['Index'][dir_key]
-        ).flatten())
-        for upd in range(first_update, last_update)
-    ]
     for idx in range(file['Index']['own'].size)
-    if (ch[dir[idx]] != ch[idx] and pc[idx] != ch[dir[idx]])
 ]
 
 
@@ -87,14 +91,24 @@ outfile = (
 pd.DataFrame.from_dict([
     {
         'Shared Resource Per Cell Pair Update' : v,
-        'Propagule Parent' : 'true' if idx == 0 else 'false',
+        'Propagule Parent' : 'true',
         'First Update' : first_update,
         'Last Update' : last_update,
         'Treatment' : ExtractTreat(filename),
         'Seed' : ExtractSeed(filename)
     }
-    for tup in zip(propagule_parent, not_propagule_parent)
+    for v in propagule_parent
     for idx, v in enumerate(tup)
+] + [
+    {
+        'Shared Resource Per Cell Pair Update' : v,
+        'Propagule Parent' : 'false',
+        'First Update' : first_update,
+        'Last Update' : last_update,
+        'Treatment' : ExtractTreat(filename),
+        'Seed' : ExtractSeed(filename)
+    }
+    for v in not_propagule_parent
 ]).to_csv(outfile, index=False)
 
 print('Output saved to', outfile)
