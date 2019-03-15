44b6cfd5f0d950ec8817aa3a0f5c9e1119a1ae73
Matthew Andres Moreno <m.more500@gmail.com>
2019-03-15 18:01:04 -0400

    Data was organized differently than assumed

--- script/ReproductiveLaborPrep.py
+++ script/ReproductiveLaborPrep.py
@@ -28,52 +28,52 @@ ntile = h5py.File(filenames[0], 'r')['Index']['own'].size
 
 files = [h5py.File(filename, 'r') for filename in filenames]
 
-def CalcContainedRate(file):
+def CalcSameChannelRate(file):
     return np.mean([
         np.sum(rep for rep in reps)
-        for ch, dirs, reps in [
+        for dir_key in file['RepCount']
+        for ch, dir, live, reps in [
                 (np.array(
                     file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
                 ).flatten(),
-                [
-                    np.array(file['Index'][dir_key]).flatten()
-                    for dir_key in file['RepCount']
-                ],
+                np.array(
+                    file['Index'][dir_key]
+                ).flatten(),
+                np.array(file['Live']['upd_'+str(upd)]).flatten(),
                 [
                     np.array(
-                        file['RepCount'][dir_key_inner][replev_key_inner]['upd_'+str(upd)]
+                        file['RepCount'][dir_key][replev_key]['upd_'+str(upd)]
                     ).flatten()
-                    for dir_key_inner in file['RepCount']
-                    for replev_key_inner in file['RepCount'][dir_key_inner]
+                    for replev_key in file['RepCount'][dir_key]
                 ])
             for upd in range(first_update, last_update)
         ]
         for idx in range(file['Index']['own'].size)
-        if all(ch[idx] == ch[dir[idx]] for dir in dirs)
+        if live[idx] and ch[idx] == ch[dir[idx]]
     ])
 
-def CalcFreeRate(file):
+def CalcDiffChannelRate(file):
     return np.mean([
         np.sum(rep for rep in reps)
-        for ch, dirs, reps in [
+        for dir_key in file['RepCount']
+        for ch, dir, live, reps in [
                 (np.array(
                     file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
                 ).flatten(),
-                [
-                    np.array(file['Index'][dir_key]).flatten()
-                    for dir_key in file['RepCount']
-                ],
+                np.array(
+                    file['Index'][dir_key]
+                ).flatten(),
+                np.array(file['Live']['upd_'+str(upd)]).flatten(),
                 [
                     np.array(
-                        file['RepCount'][dir_key_inner][replev_key_inner]['upd_'+str(upd)]
+                        file['RepCount'][dir_key][replev_key]['upd_'+str(upd)]
                     ).flatten()
-                    for dir_key_inner in file['RepCount']
-                    for replev_key_inner in file['RepCount'][dir_key_inner]
+                    for replev_key in file['RepCount'][dir_key]
                 ])
             for upd in range(first_update, last_update)
         ]
         for idx in range(file['Index']['own'].size)
-        if not all(ch[idx] == ch[dir[idx]] for dir in dirs)
+        if live[idx] and ch[idx] != ch[dir[idx]]
     ])
 
 
@@ -87,8 +87,19 @@ outfile = (
 pd.DataFrame.from_dict([
     {
         'Treatment' : treat,
-        'Reproduction Rate Contained' : CalcContainedRate(file),
-        'Reproduction Rate Free' : CalcFreeRate(file),
+        'Incoming Reproduction Rate' : CalcSameChannelRate(file),
+        'Level 1 Channel ID' : 'Matching',
+        'First Update' : first_update,
+        'Last Update' : last_update
+    }
+    for treat, file in (
+        (ExtractTreat(filename), h5py.File(filename, 'r')) for filename in tqdm(filenames)
+    )
+] + [
+    {
+        'Treatment' : treat,
+        'Incoming Reproduction Rate' : CalcDiffChannelRate(file),
+        'Level 1 Channel ID' : 'Differing',
         'First Update' : first_update,
         'Last Update' : last_update
     }
