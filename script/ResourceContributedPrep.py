84b99aacd04ede1ec373f90db72bf384b7e55b2e
Matthew Andres Moreno <m.more500@gmail.com>
2019-04-28 13:24:02 -0400

    Just focus on same versus different channel

--- script/ResourceContributedPrep.py
+++ script/ResourceContributedPrep.py
@@ -18,34 +18,13 @@ filenames = sys.argv[3:]
 # check all data is from same software source
 assert len({kn.unpack(filename)['_source_hash'] for filename in filenames}) == 1
 
-# check there's only one treatment being analyzed
-assert len({ kn.unpack(filename)['treat'] for filename in filenames }) == 1
-
-print("TREATMENT:", kn.unpack(filenames[0])['treat'])
-
 nlev = h5py.File(filenames[0], 'r').attrs['NLEV'][0]
 ntile = h5py.File(filenames[0], 'r')['Index']['own'].size
 
 files = [h5py.File(filename, 'r') for filename in filenames]
 
-res = {}
-
-res['none'] = (
-[
-    np.mean([
-        np.mean(file['ResourceContributed'][dir_key]['upd_'+str(upd)])
-        for dir_key in file['ResourceContributed']
-        for upd in range(first_update, last_update)
-    ])
-    for file in tqdm(files)
-],
-None
-)
-
-for lev_key in files[0]['Channel']:
-    res['samechannel_'+lev_key] = (
-    [
-    np.mean([
+def CalcSameChannelContrib(file):
+    res = [
         rc[idx]
         for dir_key in file['ResourceContributed']
         for rc, ch, dir in [
@@ -53,7 +32,7 @@ for lev_key in files[0]['Channel']:
                 file['ResourceContributed'][dir_key]['upd_'+str(upd)]
             ).flatten(),
             np.array(
-                file['Channel'][lev_key]['upd_'+str(upd)]
+                file['Channel']['lev_0']['upd_'+str(upd)]
             ).flatten(),
             np.array(
                 file['Index'][dir_key]
@@ -62,10 +41,11 @@ for lev_key in files[0]['Channel']:
         ]
         for idx in range(file['Index']['own'].size)
         if (ch[idx] == ch[dir[idx]])
-    ]) for file in tqdm(files)
-    ],
-    [
-    np.mean([
+    ]
+    return np.mean(res) if len(res) else 0
+
+def CalcDiffChannelContrib(file):
+    res = [
         rc[idx]
         for dir_key in file['ResourceContributed']
         for rc, ch, dir in [
@@ -82,214 +62,12 @@ for lev_key in files[0]['Channel']:
         ]
         for idx in range(file['Index']['own'].size)
         if (ch[idx] != ch[dir[idx]])
-    ]) for file in tqdm(files)
     ]
-    )
-
-res['cell_parent'] = (
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, par, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['ParentPos']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (par[idx] == dir[idx])
-    ]) for file in tqdm(files)
-],
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, par, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['ParentPos']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (par[idx] != dir[idx])
-    ]) for file in tqdm(files)
-]
-)
-
-res['cell_child'] = (
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, par, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['ParentPos']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (idx == par[dir[idx]])
-    ]) for file in tqdm(files)
-],
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, par, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['ParentPos']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (idx != par[dir[idx]])
-    ]) for file in tqdm(files)
-]
-)
-
-res['propagule_parent'] = (
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, pc, ch, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['PrevChan']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (ch[dir[idx]] != ch[idx] and pc[idx] == ch[dir[idx]])
-    ]) for file in tqdm(files)
-],
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, pc, ch, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['PrevChan']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (ch[dir[idx]] != ch[idx] and pc[idx] != ch[dir[idx]])
-    ]) for file in tqdm(files)
-]
-)
-
-res['propagule_child'] = (
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, pc, ch, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['PrevChan']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (ch[dir[idx]] != ch[idx] and ch[idx] == pc[dir[idx]])
-    ]) for file in tqdm(files)
-],
-[
-    np.mean([
-        rc[idx]
-        for dir_key in file['ResourceContributed']
-        for rc, pc, ch, dir in [
-            (np.array(
-                file['ResourceContributed'][dir_key]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['PrevChan']['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Channel']['lev_'+str(nlev-1)]['upd_'+str(upd)]
-            ).flatten(),
-            np.array(
-                file['Index'][dir_key]
-            ).flatten())
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if (ch[dir[idx]] != ch[idx] and ch[idx] != pc[dir[idx]])
-    ]) for file in tqdm(files)
-]
-)
+    return np.mean(res) if len(res) else 0
 
 print("num files:" , len(files))
 
-for k, (v,not_v) in sorted(res.items()):
-    print()
-    print("FILTER: ", k)
-    print("   YES mean/std: ", np.mean(v), "/", np.std(v))
-    if not_v:
-        print("   NOT mean/std: ", np.mean(not_v), "/", np.std(not_v))
-        print(
-            "   WELCH TEST: ",
-            stats.ttest_ind(
-                v,
-                not_v,
-                equal_var=False
-            )
-        )
-    print()
-
 outfile = kn.pack({
-    'treat' : kn.unpack(filenames[0])['treat'],
     'title' : 'resource_contributed',
     '_data_hathash_hash' : fsh.FilesHash().hash_files(filenames),
     '_script_fullcat_hash' : fsh.FilesHash(
@@ -302,18 +80,26 @@ outfile = kn.pack({
 
 pd.DataFrame.from_dict([
     {
-        'Filter' : k,
-        'Shared Resource Per Cell Pair Update' : v,
-        'Applied' : 'true' if idx == 0 else 'false',
+        'Shared Resource Per Cell Pair Update'
+            : CalcSameChannelContrib(filename),
+        'Channel Match' : 'true',
         'First Update' : first_update,
         'Last Update' : last_update,
         'Treatment' : kn.unpack(fname)['treat'],
         'Seed' : kn.unpack(fname)['seed']
     }
-    for k, tup in res.items()
-    for idx, vs in enumerate(tup)
-    if vs
-    for v, fname in zip(vs,filenames)
+    for filename in filenames
+] + [
+    {
+        'Shared Resource Per Cell Pair Update'
+            : CalcDiffChannelContrib(filename),
+        'Channel Match' : 'false',
+        'First Update' : first_update,
+        'Last Update' : last_update,
+        'Treatment' : kn.unpack(fname)['treat'],
+        'Seed' : kn.unpack(fname)['seed']
+    }
+    for filename in filenames
 ]).to_csv(outfile, index=False)
 
 print('Output saved to', outfile)
