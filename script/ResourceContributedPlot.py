3f6d67e7cd1e3098367dc18bb8f8f08259aab463
Matthew Andres Moreno <m.more500@gmail.com>
2019-04-28 15:32:03 -0400

    Make side-by-side horizontal bar plots

--- script/ResourceContributedPlot.py
+++ script/ResourceContributedPlot.py
@@ -20,37 +20,77 @@ dataframe_filename = sys.argv[1]
 
 df = pd.read_csv(dataframe_filename)
 
-df['Filter'] = df['Filter'].apply(lambda raw : {
-    'none' : 'None',
-    'samechannel_lev_0' : 'Level 0\nChannel\nMatch',
-    'samechannel_lev_1' : 'Level 1\nChannel\nMatch',
-    'cell_parent' : 'Cell\nParent',
-    'cell_child' : 'Cell\nChild',
-    'propagule_parent' : 'Propagule\nParent',
-    'propagule_child' : 'Propagule\nChild'
+df['Wave Size'] = df['Treatment'].apply(lambda raw : {
+    'wave-big__mut-a_low' : 'big',
+    'wave-big__mut-b_medlow' : 'big',
+    'wave-big__mut-c_medhigh' : 'big',
+    'wave-big__mut-d_high' : 'big',
+    'wave-big__mut-e_extreme' : 'big',
+    'wave-small__mut-a_low' : 'small',
+    'wave-small__mut-b_medlow' : 'small',
+    'wave-small__mut-c_medhigh' : 'small',
+    'wave-small__mut-d_high' : 'small',
+    'wave-small__mut-e_extreme' : 'small',
     }[raw]
 )
 
-df['State'] = df['Applied']
+df['Mutational Load'] = df['Treatment'].apply(lambda raw : {
+    'wave-big__mut-a_low' : '1',
+    'wave-big__mut-b_medlow' : '2',
+    'wave-big__mut-c_medhigh' : '3',
+    'wave-big__mut-d_high' : '4',
+    'wave-big__mut-e_extreme' : '5',
+    'wave-small__mut-a_low' : '1',
+    'wave-small__mut-b_medlow' : '2',
+    'wave-small__mut-c_medhigh' : '3',
+    'wave-small__mut-d_high' : '4',
+    'wave-small__mut-e_extreme' : '5',
+    }[raw]
+).astype('category')
 
-df = df[df['Filter'] != 'None']
+df['Channel Match'] = df['Channel Match'].astype(str)
 
-print(df)
+ax1 = plt.subplot(121)
+ax1.invert_xaxis()
 
-ax = sns.barplot(
-    x="Filter",
-    y="Shared Resource Per Cell Pair Update",
-    hue="State",
-    order=['Level 0\nChannel\nMatch', 'Level 1\nChannel\nMatch', 'Cell\nParent', 'Cell\nChild', 'Propagule\nParent', 'Propagule\nChild'],
-    # hue_order=['True','False'],
-    data=df
+sns.barplot(
+    y="Mutational Load",
+    x="Shared Resource Per Cell Pair Update",
+    hue="Channel Match",
+    hue_order=["True", "False"],
+    data=df[df["Wave Size"] == 'small']
 )
-plt.xticks(rotation=30)
 
+ax1.set_title('Small Wave')
+ax1.xaxis.label.set_visible(False)
 
-ax.get_figure().savefig(
+ax2 = plt.subplot(122)
+
+sns.barplot(
+    y="Mutational Load",
+    x="Shared Resource Per Cell Pair Update",
+    hue="Channel Match",
+    hue_order=["True", "False"],
+    data=df[df["Wave Size"] == 'big']
+).get_legend().remove()
+
+ax2.set_title('Big Wave')
+
+ax2.set_yticklabels([])
+ax2.yaxis.label.set_visible(False)
+ax2.xaxis.label.set_visible(False)
+
+ax2.get_shared_y_axes().join(ax2, ax2)
+plt.subplots_adjust(wspace=.0)
+plt.gcf().text(0.5, 0.0, 'Shared Resource Per Cell Pair Update', ha='center')
+
+bigger = max((ax1, ax2), key=lambda x: max(x.get_xlim()))
+print(bigger)
+ax1.set_xlim(0.2, 0)
+ax2.set_xlim(0, 0.2)
+
+plt.gcf().savefig(
     kn.pack({
-        'treat' : kn.unpack(filenames[0])['treat'],
         'title' : 'resource_contributed',
         '_data_hathash_hash' : fsh.FilesHash().hash_files([dataframe_filename]),
         '_script_fullcat_hash' : fsh.FilesHash(
@@ -62,5 +102,5 @@ ax.get_figure().savefig(
     }),
     transparent=True,
     bbox_inches='tight',
-    pad_inches=0
+    pad_inches=0.1
 )
