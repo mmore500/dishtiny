2715193a704231e668a96ac0593abdfa5262543c
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-08-21 12:43:19 -0400

    Update cell death script

--- script/CellDeathPrep.py
+++ script/CellDeathPrep.py
@@ -12,56 +12,37 @@ from fileshash import fileshash as fsh
 import re
 from collections import Counter
 
-KILL_THRESH = -5.0
-
-filenames = sys.argv[1:]
+first_update = int(sys.argv[1])
+last_update = int(sys.argv[2])
+filenames = sys.argv[3:]
 
 # check all data is from same software source
 assert len({kn.unpack(filename)['_source_hash'] for filename in filenames}) == 1
 
-def CauseOfDeath(file, upd):
+def CauseOfDeath(file):
 
-    deaths = [None for idx in range(file['Index']['own'].size)]
-
-    # replaced
-    for dir_key in file['RepOutgoing']:
-        rog = np.array(file['RepOutgoing'][dir_key]['upd_'+str(upd)]).flatten()
-        facing = np.array(file['Index'][dir_key]).flatten()
-
-        for idx in range(file['Index']['own'].size):
-            if rog[idx] != -1:
-                deaths[facing[idx]] = 'Replaced'
-
-    # bankrupt
-    stock = np.array(file['Stockpile']['upd_'+str(upd)]).flatten()
-
-    for idx in range(file['Index']['own'].size):
-        if stock[idx] < -5.0:
-            deaths[idx] = 'Bankrupt'
-
-    # apoptosis
-    apop = np.array(file['Apoptosis']['upd_'+str(upd)]).flatten()
-
-    for idx in range(file['Index']['own'].size):
-        if apop[idx] == 1:
-            deaths[idx] = 'Partial Apoptosis'
-        elif apop[idx] == 2:
-            deaths[idx] = 'Complete Apoptosis'
-
-    # already dead cells can't die again
-    live = np.array(file['Live']['upd_'+str(upd)]).flatten()
-
-    for idx in range(file['Index']['own'].size):
-        if not live[idx]:
-            deaths[idx] = None
+    deaths = [
+        {
+            0 : None,
+            1 : 'Apoptosis',
+            2 : 'Bankrupt',
+            3 : 'Replaced'
+        }[death[idx]]
+        for death in [
+            np.array(
+                file['Death']['upd_'+str(upd)]
+            ).flatten()
+            for upd in range(first_update, last_update)
+        ]
+        for idx in range(file['Index']['own'].size)
+    ]
 
     res = Counter(deaths)
 
     for cause in (
         'Replaced',
         'Bankrupt',
-        'Partial Apoptosis',
-        'Complete Apoptosis',
+        'Apoptosis',
         None
     ):
         if cause not in res:
@@ -70,9 +51,6 @@ def CauseOfDeath(file, upd):
     res.pop(None)
 
     return res
-
-def ExtractUpdates(file):
-    return [int(re.findall('\d+', key)[0]) for key in file['Apoptosis'].keys()]
 
 print("num files:" , len(filenames))
 
@@ -92,13 +70,11 @@ pd.DataFrame.from_dict([
         'Treatment' : kn.unpack(filename)['treat'],
         'Seed' : kn.unpack(filename)['seed'],
         'Per-Cell-Update Death Rate' : val / file['Index']['own'].size,
-        'Cause' : cause,
-    'Update' : upd
+        'Cause' : cause
     }
     for filename in tqdm(filenames)
     for file in (h5py.File(filename, 'r'),)
-    for upd in ExtractUpdates(file)
-    for cause, val in CauseOfDeath(file, upd).items()
+    for cause, val in CauseOfDeath(file).items()
 ]).to_csv(outfile, index=False)
 
 print('Output saved to', outfile)
