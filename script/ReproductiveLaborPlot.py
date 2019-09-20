580774f16a5d6f8c5b8a38cfeda1abb60f389c24
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-20 12:00:44 -0400

    Revam reproductive labor scripts

--- script/ReproductiveLaborPlot.py
+++ script/ReproductiveLaborPlot.py
@@ -15,41 +15,111 @@ from fileshash import fileshash as fsh
 matplotlib.rcParams['pdf.fonttype'] = 42
 sns.set(style='whitegrid')
 
+non_url_safe = ['"', '#', '$', '%', '&', '+',
+                ',', '/', ':', ';', '=', '?',
+                '@', '[', '\\', ']', '^', '`',
+                '{', '|', '}', '~', "'"]
+
+def slugify(text):
+    """
+    Turn the text content of a header into a slug for use in an ID
+    """
+    non_safe = [c for c in text if c in non_url_safe]
+    if non_safe:
+        for c in non_safe:
+            text = text.replace(c, '')
+    # Strip leading, trailing and multiple whitespace, convert remaining whitespace to _
+    text = u'_'.join(text.split())
+    return text
+
 dataframe_filename = sys.argv[1]
 
 df = pd.read_csv(dataframe_filename)
 
+print("Data loaded!")
+
 df['Treatment'] = df['Treatment'].apply(lambda raw : {
+    'resource-even__channelsense-no__nlev-two__mute' : 'even+blind+mute',
     'resource-even__channelsense-no__nlev-two' : 'even+blind',
     'resource-wave__channelsense-no__nlev-two' : 'blind',
     'resource-wave__channelsense-yes__nlev-onesmall' : 'small wave',
     'resource-even__channelsense-yes__nlev-two' : 'even',
     'resource-wave__channelsense-yes__nlev-onebig' : 'large wave',
-    'resource-wave__channelsense-yes__nlev-two' : 'standard'
+    'resource-wave__channelsense-yes__nlev-two' : 'nested wave'
     }[raw]
 )
 
-ax = sns.barplot(
-    x="Treatment",
-    y="Incoming Reproduction Rate",
-    hue="Level 1 Channel ID",
-    data=df
+df['Relationship Category'] = df.apply(
+    lambda x: (
+        'Neighbor' if 'Neighbor' in x['Relationship']
+        else 'Channel' if 'mate' in x['Relationship']
+        else 'Cell' if 'Cell' in x['Relationship']
+        else 'Propagule' if 'Propagule' in x['Relationship']
+        else 'Unknown'
+    ),
+    axis=1
 )
-plt.xticks(rotation=30)
 
+print("Data crunched!")
+for measure in df['Measure'].unique():
 
-ax.get_figure().savefig(
-    kn.pack({
-        'title' : 'reproductive_labor',
+    plt.clf()
+
+    g = sns.FacetGrid(
+        df.pivot_table(
+            index=[
+                'First Update', 'Last Update', 'Relationship',
+                'Relationship Category', 'Seed', 'Treatment'
+            ],
+            columns='Measure',
+            values='Value',
+            aggfunc='first'
+        ).reset_index(),
+        col="Treatment",
+        hue="Relationship Category",
+        col_wrap=4,
+        sharey=False
+    )
+
+    g.map(
+        sns.barplot,
+        "Relationship",
+        measure,
+        order=[
+            'Neighbor',
+            'Related Neighbor',
+            'Unrelated Neighbor',
+            ] + sorted([
+                r for r in df['Relationship'].unique() if 'Channelmate' in r
+            ]) + [
+            'Nonchannelmate',
+            'Cell Child',
+            'Cell Parent',
+            'Propagule Child',
+            'Propagule Parent',
+        ],
+    )
+
+    for ax in g.axes.flat:
+        for label in ax.get_xticklabels():
+            label.set_rotation(-90)
+
+    outfile = kn.pack({
+        'title' : slugify(measure),
         '_data_hathash_hash' : fsh.FilesHash().hash_files([dataframe_filename]),
         '_script_fullcat_hash' : fsh.FilesHash(
-                                            file_parcel="full_parcel",
-                                            files_join="cat_join"
-                                        ).hash_files([sys.argv[0]]),
-        '_source_hash' :kn.unpack(filename)['_source_hash'],
-        'ext' : '.pdf'
-    }),
-    transparent=True,
-    bbox_inches='tight',
-    pad_inches=0
-)
+                                        file_parcel="full_parcel",
+                                        files_join="cat_join"
+                                    ).hash_files([sys.argv[0]]),
+        '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+        'ext' : ".pdf"
+    })
+
+    plt.gcf().savefig(
+        outfile,
+        transparent=True,
+        bbox_inches='tight',
+        pad_inches=0
+    )
+
+    print("Output saved to", outfile)
--- script/ReproductiveLaborPrep.py
+++ script/ReproductiveLaborPrep.py
@@ -10,6 +10,8 @@ import os
 import pandas as pd
 from keyname import keyname as kn
 from fileshash import fileshash as fsh
+from collections import defaultdict
+from joblib import delayed, Parallel
 
 first_update = int(sys.argv[1])
 last_update = int(sys.argv[2])
@@ -18,99 +20,171 @@ filenames = sys.argv[3:]
 # check all data is from same software source
 assert len({kn.unpack(filename)['_source_hash'] for filename in filenames}) == 1
 
-files = [h5py.File(filename, 'r') for filename in filenames]
+nlev = h5py.File(filenames[0], 'r').attrs['NLEV'][0]
+ntile = h5py.File(filenames[0], 'r')['Index']['own'].size
 
-def CalcSameChannelRate(file):
-    return np.mean([
-        1 if ro[idx] != -1 else 0
-        for dir_key in file['RepOutgoing']
-        for ch, pc, dir, live, ro in [
-                (
-                np.array(
-                    file['Channel']['lev_'+str(file.attrs['NLEV'][0]-1)]['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['PrevChan']['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['Index'][dir_key]
-                ).flatten(),
-                np.array(
-                    file['Live']['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['RepOutgoing'][dir_key]['upd_'+str(upd)]
-                ).flatten()
-                )
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if live[idx] and live[dir[idx]] and ch[idx] == ch[dir[idx]]
-    ])
+def CalcRepr(filename):
+    file = h5py.File(filename, 'r')
+    # relationship type -> metric -> value
+    res = defaultdict(lambda: defaultdict(float))
 
+    nlev = int(file.attrs.get('NLEV'))
+    ncell = file['Index']['own'].size
 
-def CalcDiffChannelRate(file):
-    return np.mean([
-        1 if ro[idx] != -1 else 0
-        for dir_key in file['RepOutgoing']
-        for ch, pc, dir, live, ro in [
-                (
-                np.array(
-                    file['Channel']['lev_'+str(file.attrs['NLEV'][0]-1)]['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['PrevChan']['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['Index'][dir_key]
-                ).flatten(),
-                np.array(
-                    file['Live']['upd_'+str(upd)]
-                ).flatten(),
-                np.array(
-                    file['RepOutgoing'][dir_key]['upd_'+str(upd)]
-                ).flatten()
-                )
-            for upd in range(first_update, last_update)
-        ]
-        for idx in range(file['Index']['own'].size)
-        if live[idx] and live[dir[idx]] and ch[idx] != ch[dir[idx]]
-    ])
+    cage_group = file['CellAge']
+    live_group = file['Live']
+    ppos_group = file['ParentPos']
+    pvch_group = file['PrevChan']
+    stck_group = file['Stockpile']
+
+    chan_groups = [
+        file['Channel']['lev_' + str(idx)]
+        for idx in range(nlev)
+    ]
+
+    for dir_key in file['ResourceContributed']:
+
+        drct = np.array(file['Index'][dir_key]).flatten()
+
+        repo_group = file['RepOutgoing'][dir_key]
+
+        for upd in range(first_update, last_update):
+
+            cage = np.array(cage_group['upd_'+str(upd)]).flatten()
+            live = np.array(live_group['upd_'+str(upd)]).flatten()
+            ppos = np.array(ppos_group['upd_'+str(upd)]).flatten()
+
+            pvch = np.array(pvch_group['upd_'+str(upd)]).flatten()
+            stck = np.array(stck_group['upd_'+str(upd)]).flatten()
+
+            repo = np.array(repo_group['upd_'+str(upd)]).flatten()
+
+            chans = [
+                np.array(g['upd_'+str(upd)]).flatten()
+                for g in chan_groups
+            ]
 
 
-print("num files:" , len(files))
+            for idx in range(ncell):
+
+                # if either of the cells are not alive, there is no sharing
+                if not live[idx] or not live[drct[idx]]:
+                    continue
+
+                related = False
+
+                dest = res['Neighbor']
+                dest['Reproduction Incidence'] += (repo[idx] != -1)
+                dest['Relationship Incidence'] += 1
+                dest['Total Source Stockpile'] += stck[drct[idx]]
+
+                for lev in range(nlev):
+                    # remember, these are mutually exclusive
+                    if chans[lev][idx] == chans[lev][drct[idx]]:
+                        dest = res['Channelmate ' + str(lev)]
+                        dest['Reproduction Incidence'] += (repo[idx] != -1)
+                        dest['Relationship Incidence'] += 1
+                        dest['Total Source Stockpile'] += stck[drct[idx]]
+                        related = True
+                        break
+                else:
+                    # runs if no break is called
+                    dest = res['Nonchannelmate']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+
+                if ppos[idx] == drct[idx] and cage[idx] < cage[drct[idx]]:
+                    dest = res['Cell Parent']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+                    related = True
+                elif ppos[drct[idx]] == idx and cage[drct[idx]] < cage[idx]:
+                    dest = res['Cell Child']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+                    related = True
+                else:
+                    dest = res['Nondirect Cell Relative']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+
+                if pvch[idx] == chans[-1][drct[idx]]:
+                    dest = res['Propagule Parent']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+                    related = True
+                elif pvch[drct[idx]] == chans[-1][idx]:
+                    dest = res['Propagule Child']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+                    related = True
+                else:
+                    dest = res['Nondirect Propagule Relative']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+
+                if related:
+                    dest = res['Related Neighbor']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+                else:
+                    dest = res['Unrelated Neighbor']
+                    dest['Reproduction Incidence'] += (repo[idx] != -1)
+                    dest['Relationship Incidence'] += 1
+                    dest['Total Source Stockpile'] += stck[drct[idx]]
+
+    for data in res.values():
+        data['Reproduction Rate'] = (
+            data['Reproduction Incidence'] / data['Relationship Incidence']
+        )
+        data['Mean Source Stockpile'] = (
+            data['Total Source Stockpile'] / data['Relationship Incidence']
+        )
+        data['Relationship Frequency'] = (
+            data['Relationship Incidence']
+            / res['Neighbor']['Relationship Incidence']
+        )
+
+    return res
+
+print("num files:" , len(filenames))
 
 outfile = kn.pack({
     'title' : 'reproductive_labor',
-    '_data_hathash_hash' : fsh.FilesHash().hash_files(filenames),
+    '_data_hathash_hash' : fsh.FilesHash().hash_files(tqdm(filenames)),
     '_script_fullcat_hash' : fsh.FilesHash(
                                     file_parcel="full_parcel",
                                     files_join="cat_join"
                                 ).hash_files([sys.argv[0]]),
-    '_source_hash' : kn.unpack(filenames[0])['_source_hash'],
-    'ext' : '.csv'
+    '_source_hash' :kn.unpack(filenames[0])['_source_hash'],
+    'ext' : ".csv"
 })
 
 pd.DataFrame.from_dict([
     {
-        'Treatment' : kn.unpack(filename)['treat'],
-        'Outgoing Reproduction Rate' :
-            CalcSameChannelRate(h5py.File(filename, 'r')),
-        'Level 0 Channel ID' : 'Matching',
+        'Measure' : measure,
+        'Value' : value,
+        'Relationship' : relationship,
         'First Update' : first_update,
-        'Last Update' : last_update
-    }
-    for filename in tqdm(filenames)
-] + [
-    {
+        'Last Update' : last_update,
         'Treatment' : kn.unpack(filename)['treat'],
-        'Outgoing Reproduction Rate' :
-            CalcDiffChannelRate(h5py.File(filename, 'r')),
-        'Level 0 Channel ID' : 'Differing',
-        'First Update' : first_update,
-        'Last Update' : last_update
+        'Seed' : kn.unpack(filename)['seed']
     }
-    for filename in tqdm(filenames)
+    for res, filename in zip(
+        Parallel(n_jobs=-1)(
+            delayed(CalcRepr)(filename) for filename in tqdm(filenames)
+        ), filenames
+    )
+    for relationship, data in res.items()
+    for measure, value in data.items()
 ]).to_csv(outfile, index=False)
 
 print('Output saved to', outfile)
