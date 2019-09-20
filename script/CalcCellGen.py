f34104b39d643e9b3081fd2a10e3f9de6f8bb1a8
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-20 12:41:00 -0400

    Improve naming and parallelize script

--- script/CalcCellGen.py
+++ script/CalcCellGen.py
@@ -11,23 +11,25 @@ import matplotlib.pyplot as plt
 import os
 from keyname import keyname as kn
 from fileshash import fileshash as fsh
+from joblib import delayed, Parallel
 
-upd = int(sys.argv[1])
+update = int(sys.argv[1])
 filenames = sys.argv[2:]
 
 # check all data is from same software source
 assert len({kn.unpack(filename)['_source_hash'] for filename in filenames}) == 1
 
-def CalcMeanCellGen(file):
+def CalcMeanCellGen(filename):
+    file = h5py.File(filename, 'r')
     return np.mean([
         cgen[idx]
         for cgen, live in [
             (
             np.array(
-                file['CellGen']['upd_'+str(upd)]
+                file['CellGen']['upd_'+str(update)]
             ).flatten(),
             np.array(
-                file['Live']['upd_'+str(upd)]
+                file['Live']['upd_'+str(update)]
             ).flatten()
             )
         ]
@@ -37,28 +39,32 @@ def CalcMeanCellGen(file):
 
 df = pd.DataFrame.from_dict([
     {
-        'Treat' : treat,
-        'CellGen' : CalcMeanCellGen(file),
-        'Upd' : upd
+        'Treatment' : kn.unpack(filename)['treat'],
+        'Mean Cell Generation' : cellgen,
+        'Update' : update
     }
-    for treat, file in [
-        (kn.unpack(filename)['treat'], h5py.File(filename, 'r')) for filename in tqdm(filenames)
-    ]
+    for filename, cellgen in zip(
+        filenames,
+        Parallel(n_jobs=-1)(
+            delayed(CalcMeanCellGen)(filename) for filename in tqdm(filenames)
+        )
+    )
 ])
 
 print("num files:" , len(filenames))
 
-for treat in df['Treat'].unique():
-    print("TREAT:", treat)
+for treat in df['Treatment'].unique():
+    print("TREATMENT:", treat)
     print(
         "   ",
         "nreps:",
-        len(df.loc[df['Treat'] == treat])
+        len(df.loc[df['Treatment'] == treat])
     )
     print(
         "   ",
-        "ElapsedCellGen mean / std:",
-        np.mean(df.loc[df['Treat'] == treat]['CellGen']),
+        "Elapsed Cell Generation (mean / std):",
+        np.mean(df.loc[df['Treatment'] == treat]['Mean Cell Generation']),
         " / ",
-        np.std(df.loc[df['Treat'] == treat]['CellGen'])
+        np.std(df.loc[df['Treatment'] == treat]['Mean Cell Generation'])
     )
+    print()
