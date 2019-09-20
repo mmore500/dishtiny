9931de7b9bd809d827c466217d20d3fc1d744932
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-20 14:41:02 -0400

    Parallelize stitch frames script

--- script/AnimateStitchFrames.py
+++ script/AnimateStitchFrames.py
@@ -5,6 +5,7 @@ from keyname import keyname as kn
 from collections import defaultdict
 import pandas as pd
 from tqdm import tqdm
+from joblib import delayed, Parallel
 
 filenames = sys.argv[1:]
 
@@ -12,21 +13,28 @@ assert len({kn.unpack(filename)['_source_hash'] for filename in filenames}) == 1
 
 df = pd.DataFrame.from_dict([kn.unpack(filename) for filename in filenames])
 
+updates = [int(upd) for upd in df['update'].unique()]
+
 for treat in df['treat'].unique():
-    updates = [int(upd) for upd in df['update'].unique()]
-    for idx, update in tqdm(enumerate(sorted(updates))):
-        fil = df[(df['treat'] == treat) & (df['update'] == str(update))]
-        if len(fil['title'].unique()) == len(df['title'].unique()):
-            os.system(' '.join(
-                ['convert', '+append']
-                + fil['_'].tolist()
-                + [kn.pack({
-                    'treat' : treat,
-                    'frame' : str(idx).zfill(4),
-                    '_source_hash' : kn.unpack(filenames[0])['_source_hash'],
-                    'ext' : '.png'
-                    })]
-            ))
+
+    commands = [
+        ' '.join(
+            ['convert', '+append']
+            + fil['_'].tolist()
+            + [kn.pack({
+                'treat' : treat,
+                'frame' : str(idx).zfill(4),
+                '_source_hash' : kn.unpack(filenames[0])['_source_hash'],
+                'ext' : '.png'
+                })]
+        ) for idx, update in enumerate(sorted(updates))
+        for fil in (df[(df['treat'] == treat) & (df['update'] == str(update))],)
+        if len(fil['title'].unique()) == len(df['title'].unique())
+    ]
+
+    Parallel(n_jobs=-1)(
+        delayed(os.system)(command) for command in tqdm(commands)
+    )
 
     os.system(' '.join(
         ['ffmpeg','-framerate','10','-i']
