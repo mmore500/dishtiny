211396d6fae0f9e0e8cc1d633922cec460bac689
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-01-10 19:31:11 -0500

    Use flat data output

--- request/treat=standard/run_list.slurm.sh
+++ request/treat=standard/run_list.slurm.sh
@@ -18,8 +18,8 @@ SEED_OFFSET=1000
 TREATMENT="treat=standard"
 SEED=$((SLURM_ARRAY_TASK_ID + SEED_OFFSET))
 
-OUTPUT_DIR="/mnt/scratch/mmore500/dishtiny/"${TREATMENT}"/"${SEED}"/"
-CONFIG_DIR="/mnt/home/mmore500/dishtiny/request/"${TREATMENT}
+OUTPUT_DIR="/mnt/scratch/mmore500/dishtiny/${TREATMENT}+seed=${SEED}"
+CONFIG_DIR="/mnt/home/mmore500/dishtiny/request/${TREATMENT}"
 
 echo "   TREATMENT" $TREATMENT
 echo "   SEED" $SEED
