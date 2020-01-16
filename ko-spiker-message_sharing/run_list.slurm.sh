a4570416d41d0714dc1fd6e0092eb55d2102c5fd
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-01-15 21:48:26 -0500

    Update knockout experiments

---
+++ ko-spiker-message_sharing/run_list.slurm.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+########## Define Resources Needed with SBATCH Lines ##########
+#SBATCH --time=4:00:00
+#SBATCH --array=0-99
+#SBATCH --mem=16G
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 4
+#SBATCH --job-name ko-all-regulation
+#SBATCH --account=devolab
+#SBATCH --output="/mnt/home/mmore500/slurmlogs/slurm-%A_%a.out"
+
+################################################################################
+echo
+echo "Setup Exit and Error Traps"
+echo "--------------------------"
+################################################################################
+
+function on_exit() {
+
+  echo
+  echo "Run Exit Trap"
+  echo "-------------"
+
+  qstat -f ${SLURM_JOB_ID}
+
+  cp ${SLURM_LOGPATH} .
+
+}
+
+function on_error() {
+
+  echo
+  echo "Run Error Trap (FAIL)"
+  echo "---------------------"
+
+  echo "   EXIT STATUS ${1}"
+  echo "   LINE NO ${2}"
+
+  cp ${SLURM_LOGPATH} "/mnt/home/mmore500/err_slurmlogs"
+
+  qstat -f ${SLURM_JOB_ID}                                                     \
+    >> "/mnt/home/mmore500/err_slurmlogs/${SLURM_LOGFILE}"
+
+}
+
+trap "on_error ${$?} ${LINENO}" ERR
+trap "on_exit" EXIT
+
+################################################################################
+echo
+echo "Prepare Env Vars"
+echo "----------------"
+################################################################################
+
+SEED_OFFSET=1000
+SEED=$((SLURM_ARRAY_TASK_ID + SEED_OFFSET))
+LAST_STEP=24
+
+OUTPUT_DIR="/mnt/scratch/mmore500/ko-all-regulation/seed=${SEED}"
+CONFIG_DIR="/mnt/home/mmore500/dishtiny/ko-all-regulation/"
+SOURCE_DIR="/mnt/scratch/mmore500/dishtiny-screen/seed=${SEED}+step=${LAST_STEP}"
+
+echo "   SEED" $SEED
+echo "   LAST_STEP" $LAST_STEP
+echo "   OUTPUT_DIR" $OUTPUT_DIR
+echo "   CONFIG_DIR" $CONFIG_DIR
+
+export SLURM_LOGFILE="slurm-${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out"
+export SLURM_LOGPATH="/mnt/home/mmore500/slurmlogs/${SLURM_LOGFILE}"
+
+echo "   SLURM_LOGFILE" $SLURM_LOGFILE
+echo "   SLURM_LOGPATH" $SLURM_LOGPATH
+
+################################################################################
+echo
+echo "Setup Work Dir"
+echo "--------------"
+################################################################################
+
+rm -rf ${OUTPUT_DIR}/*  #If this is a redo, clear the dir
+mkdir -p ${OUTPUT_DIR}
+cp -r ${CONFIG_DIR}/* ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+echo "   PWD" $PWD
+
+# prepare python environment
+module purge; module load GCC/7.3.0-2.30 OpenMPI/3.1.1 Python/3.6.6
+source "/mnt/home/mmore500/myPy/bin/activate"
+
+# get seed population from preceeding run
+MAX_UPDATE=$(python3 -c "import sys; from keyname import keyname as kn; print( \
+  max(                                                                         \
+    int(kn.unpack(filename)['update'])                                         \
+    for filename in sys.argv[1:]                                               \
+  )                                                                            \
+)" "${SOURCE_DIR}/"*".json.cereal")
+POP_PATH="${SOURCE_DIR}/"*"+update=${MAX_UPDATE}+"*".json.cereal"
+POP_FILENAME=$(basename ${POP_PATH})
+KO_PATH="seedpop/id=2+${POP_FILENAME}"
+
+echo "   MAX_UPDATE" $MAX_UPDATE
+echo "   POP_PATH" $POP_PATH
+echo "   POP_FILENAME" $POP_FILENAME
+echo "   KO_PATH" $KO_PATH
+
+# copy over WT and to-be KO populations
+mkdir seedpop
+cp $POP_PATH "seedpop/id=1+${POP_FILENAME}"
+cp $POP_PATH $KO_PATH
+
+
+FILE_LENGTH=$(cat ${KO_PATH} | wc -l)
+echo FILE_LENGTH
+
+# knockout spiker components, genome by genome
+# 27,Nop
+# 80,SendBigFracResource
+# 81,SendSmallFracResource
+# 101,SendSpikeMsg
+
+for START STOP in                                                              \
+  $(sed -n '/program.*{$/=' ${KO_PATH} | tail -n +2) $FILE_LENGTH              \
+; do
+  for TARGET in 80 81 101; do
+    sed -i -- "${START},${STOP}s/\"id\": ${TARGET}\$/\"id\": 27/g" $KO_PATH
+  done
+done
+
+################################################################################
+echo
+echo "Do Work"
+echo "-------"
+################################################################################
+
+module purge; module load GCC/8.2.0-2.31.1 OpenMPI/3.1.3 HDF5/1.10.4;
+
+./dishtiny -SEED $(( ${SEED} + 25 )) >run.log 2>&1
+
+################################################################################
+echo
+echo "Done! (SUCCESS)"
+echo "---------------"
+################################################################################
