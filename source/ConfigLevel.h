d638b93b227ebceee8dd87704d041f4c02f44185
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-01-22 23:42:18 -0500

    Reparameterize for smaller, more stable groups

--- source/ConfigLevel.h
+++ source/ConfigLevel.h
@@ -9,6 +9,6 @@ EMP_BUILD_CONFIG(
   GROUP(CORE, "Core settings for a DISHTINY level"),
   VALUE(SIGNAL_RADIUS, int, 12/*std::numeric_limits<int>::max()*/, "How far can a signal propagate?"),
   VALUE(EVENT_RADIUS, int, 3, "How far from the seed are harvests beneficial?"),
-  VALUE(HARVEST_VALUE, double, 0.33, "How much resource benefit does a successful harvest grant?"),
+  VALUE(HARVEST_VALUE, double, 0.2, "How much resource benefit does a successful harvest grant?"),
   VALUE(ACTIVATION_COST, double, 0.0, "How much resource is spent to attempt a harvest?")
 )
