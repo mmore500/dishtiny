b14e35cf9c5780a72ff6eaedb74efbf981dc34fd
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-01-27 20:22:46 -0500

    Gut master branch
    This makes clear that master is just for hosting documentation
    and that actual content lives on project-specific branches

--- script/ShovelJobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# we will shovel successfully completed jobs into this directory
-mkdir -p confirmed
-
-# launch 250 parallel job submissions
-for f in $(ls | head -250);
-  # only move jobs to confirmed directory on successful submission
-  do (python /mnt/home/mmore500/dist_qsub/dist_qsub.py $f --nocheckpoint && mv $f confirmed && echo $f "success") & done
--- source/ChannelPack.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "base/vector.h"
-
-#include "Config.h"
-
-using ChannelPack = emp::vector<Config::chanid_t>;
