e7323159a4a0927ec079b1fb5aec4d06fa7cd340
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-02-13 18:19:42 -0500

    Fix bash syntax

--- match-local/run_list.slurm.sh
+++ match-local/run_list.slurm.sh
@@ -103,16 +103,11 @@ module purge; module load GCC/8.2.0-2.31.1 OpenMPI/3.1.3 HDF5/1.10.4;
 
 export OMP_NUM_THREADS=2
 
-    ./dishtiny                                                                 \
-      -SEED $SEED                                                              \
-      -SEED_POP $SEED                                                          \
-      >"title=run+seed=${SEED}+ext=.log" 2>&1 &
+./dishtiny                                                                 \
+  -SEED $SEED                                                              \
+  -SEED_POP $SEED                                                          \
+  >"title=run+seed=${SEED}+ext=.log" 2>&1
 
-  done
-
-  wait
-
-done
 
 ################################################################################
 echo
