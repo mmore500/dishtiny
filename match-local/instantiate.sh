9024028fdd2a15f4fe9529e4f51a1773641f40e5
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-02-16 14:41:41 -0500

    Remove extraneous newline

--- match-local/instantiate.sh
+++ match-local/instantiate.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-for treat in $(ls match-local/*.tar.gz | grep -o 'batch.*+' | sed 's/.$//'); do echo $treat; export treat=$treat; j2 "match-local
-/run_list.slurm.sh.jinja" -o "match-local/instantiations/treat=${treat}+ext=.slurm.sh"; done
+for treat in $(ls match-local/*.tar.gz | grep -o 'batch.*+' | sed 's/.$//'); do echo $treat; export treat=$treat; j2 "match-local/run_list.slurm.sh.jinja" -o "match-local/instantiations/treat=${treat}+ext=.slurm.sh"; done
