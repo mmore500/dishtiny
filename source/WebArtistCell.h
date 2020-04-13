5999e7c8e3607826221d2d60e08dc46927987b77
rodsan0 <rodsan0@users.noreply.github.com>
2020-04-13 10:55:01 -0400

    started move of generic functions to WebArtistBase class

--- source/WebArtistCell.h
+++ source/WebArtistCell.h
@@ -16,20 +16,12 @@ class WebArtistCell : public WebArtistBase {
 
 private:
 
-  const std::string name;
-
-  UI::Canvas canvas;
-  UI::Document* viewer;
-  UI::DocuExtras description;
-
   std::function<std::optional<T>(size_t)> getter;
   std::function<std::string(std::optional<T>)> renderer;
 
   std::function<std::string(std::optional<T>,std::optional<T>)> divider;
 
-  const Config &cfg;
 
-  size_t last_update;
 
 public:
 
@@ -41,88 +33,36 @@ public:
     std::function<std::string(std::optional<T>)> renderer_,
     const Config &cfg_,
     std::function<std::string(std::optional<T>,std::optional<T>)> divider_=[](std::optional<T>,std::optional<T>){ return "gray"; }
-  ) : name(name_)
-  , canvas(
-    std::min(GetViewPortSize() - 100, 500),
-    std::min(GetViewPortSize() - 100, 500)
-  )
-  , viewer(&viewer_)
-  , description(emp::to_string(emp::slugify(description_), "-key"))
-  , getter(getter_)
-  , renderer(renderer_)
-  , divider(divider_)
-  , cfg(cfg_)
-  , last_update(std::numeric_limits<size_t>::max())
-  {
-    viewer_ << UI::Div(
-      emp::slugify(emp::to_string(name, "card-holder"))
-    ) << UI::Div().SetAttr(
-        "class", "card text-center"
-    ).SetAttr(
-        "style", emp::to_string(
-        "width: ",
-        std::min(GetViewPortSize() - 100, 500) + 50,
-        "px;"
-      )
-    ) << UI::Div(
-      emp::slugify(emp::to_string(name, "card-header"))
-    ).SetAttr(
-      "class", "card-header"
-    ) <<  name << UI::Close(
-      emp::slugify(emp::to_string(name, "card-header"))
-    ) << UI::Div().SetAttr(
-      "class", "card-body"
-    ) << canvas;
-  }
-
-  // for use as background in WebArtistConnection
-  WebArtistCell(
-    std::string name_,
-    std::string description_,
-    UI::Canvas &canvas_,
-    std::function<std::optional<T>(size_t)> getter_,
-    std::function<std::string(std::optional<T>)> renderer_,
-    const Config &cfg_,
-    std::function<std::string(std::optional<T>,std::optional<T>)> divider_=[](std::optional<T>,std::optional<T>){ return "gray"; }
-  ) : name(name_)
-  , canvas(canvas_)
-  , description(emp::to_string(emp::slugify(description_), "-key"))
-  , getter(getter_)
-  , renderer(renderer_)
-  , divider(divider_)
-  , cfg(cfg_)
-  , last_update(std::numeric_limits<size_t>::max())
-  { ; }
-
-  void Deactivate() {
-    viewer->Div(
-      emp::slugify(emp::to_string(name, "card-holder"))
-    ).SetAttr("class", "collapse");
-    description.SetCSS("display", "none");
-  }
-
-  void Activate() {
-    const static auto boot_time {std::chrono::system_clock::now()};
-    viewer->Div(
-      emp::slugify(emp::to_string(name, "card-holder"))
-    ).SetAttr(
-      "class",
-      ""
-    ).SetCSS(
-      "order",
-      emp::to_string(
-        std::chrono::duration_cast<std::chrono::milliseconds>(
-           std::chrono::system_clock::now()  - boot_time
-        ).count()
-      )
-    );
-    description.SetCSS("display", "initial");
-  }
-
-  void Toggle() {
-    if (description.GetCSS("display") == "none") Activate();
-    else Deactivate();
-  }
+  ) : WebArtistBase(
+        name_,
+        description_,
+        viewer_,
+        cfg_
+      ),
+      getter(getter_),
+      renderer(renderer_),
+      divider(divider_)
+    {
+      viewer_ << UI::Div(
+        emp::slugify(emp::to_string(name, "card-holder"))
+      ) << UI::Div().SetAttr(
+          "class", "card text-center"
+      ).SetAttr(
+          "style", emp::to_string(
+          "width: ",
+          std::min(GetViewPortSize() - 100, 500) + 50,
+          "px;"
+        )
+      ) << UI::Div(
+        emp::slugify(emp::to_string(name, "card-header"))
+      ).SetAttr(
+        "class", "card-header"
+      ) <<  name << UI::Close(
+        emp::slugify(emp::to_string(name, "card-header"))
+      ) << UI::Div().SetAttr(
+        "class", "card-body"
+      ) << canvas;
+    }
 
   std::string GetName() const { return name; }
 
