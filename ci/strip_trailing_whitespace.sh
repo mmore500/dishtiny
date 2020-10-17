98c20b047ea290f7417287fa9f0459cfd5259405
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-17 18:53:53 -0400

    Strip empty lines at end of files

--- ci/strip_trailing_whitespace.sh
+++ ci/strip_trailing_whitespace.sh
@@ -4,5 +4,10 @@ TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*
 
 for filename in $TARGETS
 do
+  # end of files
+  # adapted from https://unix.stackexchange.com/a/81687
+  sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $filename
+
+  # end of lines
   sed -i 's/\s*$//g' $filename
 done
