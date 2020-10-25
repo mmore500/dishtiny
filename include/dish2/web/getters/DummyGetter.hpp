8b7b997b454788b97df8bb06be8fd710491942d5
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-25 18:46:06 -0400

    Implement and test DummyGetter

---
+++ include/dish2/web/getters/DummyGetter.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef DISH2_WEB_GETTERS_DUMMYGETTER_HPP_INCLUDE
+#define DISH2_WEB_GETTERS_DUMMYGETTER_HPP_INCLUDE
+
+#include "../../config/cfg.hpp"
+
+namespace dish2 {
+
+template<typename T>
+class DummyGetter {
+
+  T val;
+
+public:
+
+  using value_type = T;
+
+  DummyGetter(const T& val_=T{}) : val(val_) {}
+
+  const value_type& Get(
+    const size_t cell_idx, const size_t cardinal_idx=0
+  ) const { return val; }
+
+  size_t GetNumCells() const { return dish2::cfg.N_CELLS(); }
+
+  size_t GetNumCardinals( const size_t cell_idx=0 ) const { return 1; }
+
+};
+
+} // namespace dish2
+
+#endif // #ifndef DISH2_WEB_GETTERS_DUMMYGETTER_HPP_INCLUDE
