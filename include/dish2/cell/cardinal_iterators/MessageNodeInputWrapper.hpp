a2616bffac7232a9aac56c727b12b5d5346fa752
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-31 14:16:29 -0400

    Implement inter message launching service

---
+++ include/dish2/cell/cardinal_iterators/MessageNodeInputWrapper.hpp
@@ -0,0 +1,71 @@
+#pragma once
+#ifndef DISH2_CELL_CARDINAL_ITERATORS_MESSAGENODEINPUTWRAPPER_HPP_INCLUDE
+#define DISH2_CELL_CARDINAL_ITERATORS_MESSAGENODEINPUTWRAPPER_HPP_INCLUDE
+
+#include "../../../../third-party/Empirical/source/base/vector.h"
+
+#include "../Cardinal.hpp"
+
+namespace dish2 {
+
+template<typename Spec>
+class MessageNodeInputWrapper
+: public emp::vector<Cardinal<Spec>>::iterator {
+
+  using parent_t = typename emp::vector<Cardinal<Spec>>::iterator;
+
+public:
+
+  MessageNodeInputWrapper() = default;
+
+  MessageNodeInputWrapper(const parent_t& in) : parent_t(in) {}
+
+  using value_type = typename Cardinal<Spec>::message_node_input_t;
+  using pointer = value_type*;
+  using reference = value_type&;
+
+  value_type& operator*() {
+    return parent_t::operator*().message_node_input;
+  }
+
+  value_type* operator->() { return &operator*(); }
+
+  MessageNodeInputWrapper& operator++() {
+    parent_t::operator++();
+    return *this;
+  }
+
+  MessageNodeInputWrapper operator++(int) {
+    const auto res = *this;
+    operator++();
+    return res;
+  }
+
+  MessageNodeInputWrapper& operator--() {
+    parent_t::operator--();
+    return *this;
+  }
+
+  MessageNodeInputWrapper operator--(int) {
+    MessageNodeInputWrapper res{ *this };
+    res -= 1;
+    return res;
+  }
+
+  MessageNodeInputWrapper operator+(const size_t rhs) {
+    MessageNodeInputWrapper res{ *this };
+    res += rhs;
+    return res;
+  }
+
+  MessageNodeInputWrapper operator-(const size_t rhs) {
+    MessageNodeInputWrapper res{ *this };
+    res -= rhs;
+    return res;
+  }
+
+};
+
+} // namespace dish2
+
+#endif // #ifndef DISH2_CELL_CARDINAL_ITERATORS_MESSAGENODEINPUTWRAPPER_HPP_INCLUDE
--- include/dish2/services/InterMessageLaunchingService.hpp
+++ include/dish2/services/InterMessageLaunchingService.hpp
@@ -2,9 +2,11 @@
 #ifndef DISH2_SERVICES_INTERMESSAGELAUNCHINGSERVICE_HPP_INCLUDE
 #define DISH2_SERVICES_INTERMESSAGELAUNCHINGSERVICE_HPP_INCLUDE
 
+#include "../../../third-party/conduit/include/uitsl/algorithm/for_each.hpp"
 #include "../../../third-party/conduit/include/uitsl/math/shift_mod.hpp"
 
 #include "../cell/cardinal_iterators/CpuWrapper.hpp"
+#include "../cell/cardinal_iterators/MessageNodeInputWrapper.hpp"
 #include "../config/cfg.hpp"
 
 namespace dish2 {
@@ -24,9 +26,17 @@ struct InterMessageLaunchingService {
 
     using spec_t = typename Cell::spec_t;
 
-    static uitsl::WarnOnce warning{
-      "TODO InterMessageLaunchingService unimplemented"
-    };
+    uitsl::for_each(
+      cell.template begin< dish2::MessageNodeInputWrapper<spec_t> >(),
+      cell.template end< dish2::MessageNodeInputWrapper<spec_t> >(),
+      cell.template begin< dish2::CpuWrapper<spec_t> >(),
+      []( auto& message_input, auto& cpu ) {
+        while(
+          message_input.TryStep()
+          && cpu.TryLaunchCore( message_input.Get(), 1)
+        );
+      }
+    );
 
   }
 
