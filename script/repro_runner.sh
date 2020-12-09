8f3f1b629f8aafc90fcd6385416e8c09df6c1e96
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-09 00:06:04 -0500

    Fixup rerun repo checkout

--- script/repro_runner.sh
+++ script/repro_runner.sh
@@ -194,8 +194,12 @@ function on_exit {
   echo "#!/bin/bash" > "${rerun}"
   echo "rm -rf ${REPRO_ID} && mkdir ${REPRO_ID} && cd ${REPRO_ID}" \
     >> "${rerun}"
-  echo "git clone https://github.com/${arg_username}/${arg_slug}" >> "${rerun}"
-  echo "git -C ${arg_slug} checkout -q --recurse-submodules ${repo_sha}" >> "${rerun}"
+  echo "mkdir ${arg_slug}"
+  echo "git -C ${arg_slug} init"
+  echo "git -C ${arg_slug} remote add origin https://github.com/${arg_username}/${arg_slug}.git"
+  echo "git -C ${arg_slug} fetch --depth 1 origin ${repo_sha}"
+  echo "git -C ${arg_slug} checkout FETCH_HEAD"
+  echo "git -C ${arg_slug} submodule update --init --recursive --depth 1"
   echo "singularity shell docker://${arg_username}/${arg_slug}@sha256:${dockerfile_sha} \
 << END_OF_HEREDOC
 ${INPUT}
