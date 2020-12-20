6fbc3a5a4e8c13d9a5581953a0b4e9762bdd86c0
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-19 21:42:23 -0500

    Make print_assets web-safe

--- include/dish2/config/print_assets.hpp
+++ include/dish2/config/print_assets.hpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "../../../third-party/conduit/include/uitsl/debug/list_cwd.hpp"
 #include "../../../third-party/conduit/include/uitsl/debug/err_verify.hpp"
 #include "../../../third-party/Empirical/include/emp/base/macros.hpp"
 
@@ -18,10 +19,14 @@ void print_assets() {
   std::cout << "|   What assets am I working with?    |" << std::endl;
   std::cout << "======================================" << std::endl;
 
-  // hash all files, excluding source directory
-  uitsl::err_verify( std::system(
-    "find . -path ./dishtiny -prune -o -type f -exec md5sum {} +"
-  ) );
+  #ifndef __EMSCRIPTEN__
+    // hash all files, excluding source directory
+    uitsl::err_verify( std::system(
+      "find . -path ./dishtiny -prune -o -type f -exec md5sum {} +"
+    ) );
+  #else
+    uitsl::list_cwd();
+  #endif // #ifndef __EMSCRIPTEN__
 
   std::cout << "=======================================\n" << std::endl;
 
