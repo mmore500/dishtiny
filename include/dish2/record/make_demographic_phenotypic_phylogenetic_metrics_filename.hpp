caf4ea51d98f5abfde3f63ad7bfcd9038228a297
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-21 16:34:18 -0500

    Fix indentation

--- include/dish2/record/make_demographic_phenotypic_phylogenetic_metrics_filename.hpp
+++ include/dish2/record/make_demographic_phenotypic_phylogenetic_metrics_filename.hpp
@@ -12,30 +12,30 @@
 
 namespace dish2 {
 
-  std::string make_demographic_phenotypic_phylogenetic_metrics_filename(
-    const size_t thread_idx
-  ) {
-    auto keyname_attributes = emp::keyname::unpack_t{
-      {"a", "demographic_phenotypic_phylogenetic_metrics"},
-      {"source", EMP_STRINGIFY(DISHTINY_HASH_)},
-      {"thread", emp::to_string(thread_idx)},
-      {"ext", ".csv"}
-    };
+std::string make_demographic_phenotypic_phylogenetic_metrics_filename(
+  const size_t thread_idx
+) {
+  auto keyname_attributes = emp::keyname::unpack_t{
+    {"a", "demographic_phenotypic_phylogenetic_metrics"},
+    {"source", EMP_STRINGIFY(DISHTINY_HASH_)},
+    {"thread", emp::to_string(thread_idx)},
+    {"ext", ".csv"}
+  };
 
-    if ( std::getenv("REPRO_ID") ) {
-      keyname_attributes[ "repro" ] = std::getenv("REPRO_ID");
-    }
-
-    if ( cfg.SERIES() != std::numeric_limits<size_t>::max() ) {
-      keyname_attributes[ "series" ] = emp::to_string( cfg.SERIES() );
-    }
-
-    if ( cfg.STINT() != std::numeric_limits<size_t>::max() ) {
-      keyname_attributes[ "stint" ] = emp::to_string( cfg.STINT() );
-    }
-
-    return emp::keyname::pack( keyname_attributes );
+  if ( std::getenv("REPRO_ID") ) {
+    keyname_attributes[ "repro" ] = std::getenv("REPRO_ID");
   }
+
+  if ( cfg.SERIES() != std::numeric_limits<size_t>::max() ) {
+    keyname_attributes[ "series" ] = emp::to_string( cfg.SERIES() );
+  }
+
+  if ( cfg.STINT() != std::numeric_limits<size_t>::max() ) {
+    keyname_attributes[ "stint" ] = emp::to_string( cfg.STINT() );
+  }
+
+  return emp::keyname::pack( keyname_attributes );
+}
 
 } // namespace dish2
 
