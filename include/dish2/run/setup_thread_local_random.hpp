40f444ffdc582f6dac03435152366f934984e6a4
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-30 23:38:46 -0500

    Incorporate proc id into tlrandom seeding

--- include/dish2/run/setup_thread_local_random.hpp
+++ include/dish2/run/setup_thread_local_random.hpp
@@ -6,6 +6,7 @@
 #include <limits>
 
 #include "../../../third-party/conduit/include/uitsl/debug/safe_cast.hpp"
+#include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/Empirical/include/emp/base/vector.hpp"
 #include "../../../third-party/Empirical/include/emp/math/Random.hpp"
 #include "../../../third-party/Empirical/include/emp/meta/meta.hpp"
@@ -34,7 +35,7 @@ void setup_thread_local_random( const size_t thread_idx ) {
   std::cout  << std::endl;
 
   const size_t hash = dish2::sha256_reduce( emp::vector< size_t >{
-    preseed, thread_idx
+    preseed, thread_idx, uitsl::safe_cast<size_t>( uitsl::get_proc_id() )
   } );
 
   // seed >= 1 so that rng is seeded deterministically
