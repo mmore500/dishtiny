e919be0bc52a79b23fd5d453c41126ded0b36eae
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-11 11:34:34 -0500

    Patch up OpLibrary stub test

--- tests/dish2/operations/OpLibrary.cpp
+++ tests/dish2/operations/OpLibrary.cpp
@@ -3,7 +3,8 @@
 #include "Catch/single_include/catch2/catch.hpp"
 
 #include "dish2/operations/OpLibrary.hpp"
+#include "dish2/spec/Spec.hpp"
 
 TEST_CASE("Test OpLibrary") {
-  dish2::OpLibrary{};
+  dish2::OpLibrary<dish2::Spec>{};
 }
