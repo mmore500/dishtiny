fdb67b75cfbc9b68b599e96f4c2078c92afe90f1
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-11 17:20:57 -0500

    More workaround for stdfs in Docker container

--- include/dish2/config/setup_config.hpp
+++ include/dish2/config/setup_config.hpp
@@ -8,13 +8,18 @@
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 #include "../../../third-party/Empirical/include/emp/config/ArgManager.hpp"
 
+#include "../utility/file_exists.hpp"
+
 #include "cfg.hpp"
 
 namespace dish2 {
 
 void setup_config( emp::ArgManager& arg_manager ) {
 
-  if ( std::filesystem::exists("dishtiny.cfg") ) {
+  // std::filesystem::exists is failing inside Docker container
+  // so use stat as a backup for now
+  // if ( std::filesystem::exists("dishtiny.cfg") ) {
+  if ( dish2::file_exists("dishtiny.cfg") ) {
     std::cout << "reading configuration from dishtiny.cfg" << std::endl;
     dish2::cfg.Read( "dishtiny.cfg" );
   } else std::cout << "no configuration file found" << std::endl;
