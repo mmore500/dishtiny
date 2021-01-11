5801fd449f74210085fc20d1a824881b34a0d414
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-11 16:22:30 -0500

    Do workaround for stdfs create_dirs in container

--- include/dish2/record/global_records_initialize.hpp
+++ include/dish2/record/global_records_initialize.hpp
@@ -4,13 +4,22 @@
 
 #include <cstdlib>
 
+#include <sys/stat.h>
+
+#include "../../../third-party/conduit/include/uitsl/debug/err_audit.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 
 namespace dish2 {
 
 void global_records_initialize() {
 
-  std::filesystem::create_directories("./drawings");
+  // std::filesystem::create_directories is failing inside Docker container
+  // so use mkdir as a backup for now
+  const std::filesystem::path path{ "./drawings" };
+  uitsl::err_audit( mkdir( path.c_str(), 0755 ) );
+  emp_assert( std::filesystem::exists(path) );
+
+  // std::filesystem::create_directories("./drawings");
 
 }
 
