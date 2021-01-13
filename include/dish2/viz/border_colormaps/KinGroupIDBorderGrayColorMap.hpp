8f2b9fc25d556c90930f09438ac2eb0eca045d7a
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-12 23:39:49 -0500

    fixup! Fixup PcaRegulationArtist

---
+++ include/dish2/viz/border_colormaps/KinGroupIDBorderGrayColorMap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef DISH2_VIZ_BORDER_COLORMAPS_KINGROUPIDBORDERGRAYCOLORMAP_HPP_INCLUDE
+#define DISH2_VIZ_BORDER_COLORMAPS_KINGROUPIDBORDERGRAYCOLORMAP_HPP_INCLUDE
+
+#include <string>
+#include <utility>
+
+#include "../../../../third-party/Empirical/include/emp/math/math.hpp"
+#include "../../../../third-party/Empirical/include/emp/web/color_map.hpp"
+
+namespace dish2 {
+
+struct KinGroupIDBorderGrayColorMap {
+
+  template<typename... Args>
+  KinGroupIDBorderGrayColorMap( Args&&... ){}
+
+  template<typename ValueType>
+  std::string Paint(const ValueType& in1, const ValueType& in2) const {
+    const auto& v1 = in1.data;
+    const auto& v2 = in2.data;
+
+    if (v1[0] == v2[0]) return "transparent";
+    else if (v1.size() > 1 && v1[1] == v2[1]) return "lightgray";
+    else return "gray";
+  }
+
+};
+
+} // namespace dish2
+
+#endif // #ifndef DISH2_VIZ_BORDER_COLORMAPS_KINGROUPIDBORDERGRAYCOLORMAP_HPP_INCLUDE
