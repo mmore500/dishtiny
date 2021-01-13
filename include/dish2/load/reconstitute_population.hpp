9b75fd777f383d4e151daf21544e4dcf1c729bd1
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-13 01:00:59 -0500

    Search for eligible pop sources, not hard filename

--- include/dish2/load/reconstitute_population.hpp
+++ include/dish2/load/reconstitute_population.hpp
@@ -6,7 +6,9 @@
 
 #include "../../../third-party/cereal/include/cereal/archives/binary.hpp"
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
+#include "../../../third-party/conduit/include/uitsl/utility/keyname_directory_filter.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
+#include "../../../third-party/Empirical/include/emp/base/always_assert.hpp"
 #include "../../../third-party/Empirical/include/emp/base/vector.hpp"
 #include "../../../third-party/Empirical/include/emp/tools/keyname_utils.hpp"
 #include "../../../third-party/Empirical/include/emp/tools/string_utils.hpp"
@@ -27,18 +29,24 @@ void reconstitute_population(
 
   {
 
-    const std::filesystem::path expected_population_path = emp::keyname::pack({
+    const auto eligible_population_paths = uitsl::keyname_directory_filter({
       {"a", "population"},
+      {"proc", emp::to_string( uitsl::get_proc_id() )},
       {"thread", emp::to_string( thread_idx )},
       {"ext", ".bin"}
     });
 
-    std::fstream fs( expected_population_path );
+    emp_always_assert(
+      eligible_population_paths.size() == 1,
+      eligible_population_paths.size(), eligible_population_paths
+    );
+
+    std::fstream fs( eligible_population_paths.front() );
     cereal::BinaryInputArchive iarchive( fs );
     iarchive( reconstituted );
 
     std::cout << "proc " << uitsl::get_proc_id() << " thread " << thread_idx
-      << " reconstituted " << expected_population_path << std::endl;
+      << " reconstituted " << eligible_population_paths.front() << std::endl;
 
   }
 
