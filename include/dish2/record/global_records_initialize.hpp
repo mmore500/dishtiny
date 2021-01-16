92e315766c4d7f4d39f08ffd370cccdc9b02a3db
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-16 10:44:11 -0500

    Switch back to using std::filesystem
    Now that we've debugged in in the Docker container!

--- include/dish2/record/global_records_initialize.hpp
+++ include/dish2/record/global_records_initialize.hpp
@@ -7,11 +7,9 @@
 
 #include <sys/stat.h>
 
-#include "../../../third-party/conduit/include/uitsl/debug/err_audit.hpp"
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
-
-#include "../utility/path_exists.hpp"
+#include "../../../third-party/Empirical/include/emp/base/assert.hpp"
 
 namespace dish2 {
 
@@ -20,11 +18,9 @@ void global_records_initialize() {
   // std::filesystem::create_directories is failing inside Docker container
   // so use mkdir as a backup for now
   const std::string path{ "./drawings" };
-  if ( uitsl::is_root() && !dish2::path_exists( path ) ) {
-    uitsl_err_audit( mkdir( path.c_str(), 0755 ) );
+  if ( uitsl::is_root() && !std::filesystem::exists( path ) ) {
+    emp_always_assert( std::filesystem::create_directories("./drawings") );
   }
-
-  // std::filesystem::create_directories("./drawings");
 
 }
 
