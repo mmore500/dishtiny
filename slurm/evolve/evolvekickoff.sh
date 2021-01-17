3cfdf9a610fa2fbd3368d959ca46232cbbeb620c
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-17 13:57:48 -0500

    Run evolvekickoff in repro container

--- slurm/evolve/evolvekickoff.sh
+++ slurm/evolve/evolvekickoff.sh
@@ -19,29 +19,48 @@ shift
 
 SERIES="${@}"
 
-JOB_TEMPLATE="$(mktemp)"
-curl -o "${JOB_TEMPLATE}" "https://raw.githubusercontent.com/mmore500/dishtiny/${REPO_SHA}/slurm/evolve/evolvejob.slurm.sh.jinja"
+# set up and jump into temporary work directory
+cd "$(mktemp -d)"
 
-source ~/pyenv/bin/activate || :
+# curl repro_runner.sh script into to a temporary file
+REPRO_RUNNER="$(mktemp)"
+curl -o "${REPRO_RUNNER}" "https://raw.githubusercontent.com/mmore500/dishtiny/${REPO_SHA}/script/repro_runner.sh"
+chmod +x "${REPRO_RUNNER}"
 
-for just_one_series in "${SERIES}"; do
+################################################################################
+echo
+echo "Run Job with repro_runner.sh"
+echo "--------------------------------"
+################################################################################
 
-  JOB_SCRIPT="$(mktemp)"
+"${REPRO_RUNNER}" \
+  -p dnh2v -u mmore500 -s dishtiny \
+  --repo_sha "${REPO_SHA}" --container_tag "${CONTAINER_TAG}" \
+  << REPRO_RUNNER_HEREDOC_EOF
 
-  echo "series ${just_one_series}"
-  echo "JOB_SCRIPT ${JOB_SCRIPT}"
 
-  j2 --format=yaml -o "${JOB_SCRIPT}" "${JOB_TEMPLATE}" << J2_HEREDOC_EOF
-container_sha: "${CONTAINER_SHA}"
+source ~/.secrets.sh || :
+
+for just_one_series in ${SERIES}; do
+
+  JOB_SCRIPT="\$(mktemp)"
+
+  echo "series \${just_one_series}"
+  echo "JOB_SCRIPT \${JOB_SCRIPT}"
+
+  j2 --format=yaml -o "${JOB_SCRIPT}" "dishtiny/slurm/evolve/evolvejob.slurm.sh.jinja" << J2_HEREDOC_EOF
+container_tag: "${CONTAINER_TAG}"
 repo_sha: "${REPO_SHA}"
-series: "${just_one_series}"
+series: "\${just_one_series}"
 stint: "${STINT}"
 J2_HEREDOC_EOF
 
   for retry in {1..10}; do
-    sbatch "${JOB_SCRIPT}" && echo "  job submit success" && break \
-      || (echo "retrying job submit (${retry})" && sleep $((RANDOM % 10)))
-    if ((${retry}==10)); then echo "job submit fail" && exit 1; fi
+    sshpass -p "${HOST_PASSWORD}" ssh "${HOST_USERNAME}@\$(hostname)" sbatch "\${JOB_SCRIPT}" && echo "  job submit success" && break \
+      || (echo "retrying job submit (\${retry})" && sleep \$((RANDOM % 10)))
+    if ((\${retry}==10)); then echo "job submit fail" && exit 1; fi
   done &
 
 done
+
+REPRO_RUNNER_HEREDOC_EOF
