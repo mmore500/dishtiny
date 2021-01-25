f5031ff84080a4ee694941a23fbf2d42a738c6b1
mmore500 <mmore500.login@gmail.com>
2021-01-25 05:56:18 +0000

    Deploying to gh-pages from  @ 52c2d740c89899cd26db38028aff13ba0ddd7645 🚀

---
+++ master/make_html.py
@@ -0,0 +1,53 @@
+import toml
+import jinja2
+from markdown2 import Markdown
+
+INPUT = 'keys.toml'
+OUTPUT = 'index.html'
+TEMPLATE = "template.html.jinja"
+DIR = "web/"
+
+# load our TOML file
+with open(DIR + INPUT, 'r') as file:
+    data = toml.load(file)
+
+# process and generate badge
+def make_badge(str, color):
+    # it's a badge!
+
+    # determine brightness to decide background color
+    # based on https://stackoverflow.com/a/3943023
+    (r, g, b) = (color[1:3], color[3:5], color[5:7])
+    colors = [int(x, 16) for x in (r, g, b)]
+
+    L = 0.299 * colors[0] + 0.587 * colors[1] + 0.114 * colors[2]
+
+    if L > 186:
+        type = "badge badge-light"
+    else:
+        type = "badge badge-dark"
+
+    # for color string
+    color_str = "background-color: " + color + ";"
+
+    # form badge!
+    badge = '<span class="' + type + '" style="' + color_str + '">' + str + '</span>'
+
+    return badge
+
+# instantiate Markdown
+markdown = Markdown()
+
+# find template, adapted from https://stackoverflow.com/a/38642558
+template_loader = jinja2.FileSystemLoader(searchpath=DIR)
+template_env = jinja2.Environment(loader=template_loader)
+# load template
+template = template_env.get_template(TEMPLATE)
+# output templated html
+# we pass it our data, our markdown instance, and eval() and make_badge(),
+# since they are all used by it
+outputText = template.render(data=data, markdown=markdown, eval=eval, make_badge=make_badge)
+
+# output to file
+with open(DIR + OUTPUT, 'wb+') as out:
+    out.write( outputText.encode('utf8') )
