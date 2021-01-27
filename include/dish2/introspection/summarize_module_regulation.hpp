596445687ff95fbf4916ce37b6624e7d1cc6e7c0
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-27 13:55:08 -0500

    Add expoed regulators to module regulation summary

--- include/dish2/introspection/summarize_module_regulation.hpp
+++ include/dish2/introspection/summarize_module_regulation.hpp
@@ -20,34 +20,55 @@ hopca::Matrix summarize_module_regulation(
 ) {
 
   const size_t max_modules = dish2::get_maximum_module_count<Spec>( world );
+  // each module potentialy has a protected and an exposed regulator
+  const size_t max_regulation_dims = 2 * max_modules;
   const size_t num_live_cardinals = dish2::count_live_cardinals<Spec>( world );
   const auto& population = world.population;
 
   emp::vector<double> res;
+  res.reserve( num_live_cardinals * max_regulation_dims );
 
   std::for_each(
     dish2::LiveCellIterator<Spec>::make_begin( population ),
     dish2::LiveCellIterator<Spec>::make_end( population ),
     [&]( const auto& cell ){ for (const auto& cardinal : cell) {
 
-      auto cardinal_module_regulation = sgpl::summarize_module_regulation(
-        cardinal.cpu,
-        cell.genome->program
-      );
-
+      auto protected_cardinal_module_regulation
+        = sgpl::summarize_module_regulation(
+          cardinal.cpu,
+          cell.genome->program,
+          0
+        );
       // pad with zeros if necessary
-      cardinal_module_regulation.resize( max_modules );
+      protected_cardinal_module_regulation.resize( max_modules );
 
       std::copy(
-        std::begin( cardinal_module_regulation ),
-        std::end( cardinal_module_regulation ),
+        std::begin( protected_cardinal_module_regulation ),
+        std::end( protected_cardinal_module_regulation ),
+        std::back_inserter( res )
+      );
+
+      auto exposed_cardinal_module_regulation
+        = sgpl::summarize_module_regulation(
+          cardinal.cpu,
+          cell.genome->program,
+          1
+        );
+      // pad with zeros if necessary
+      exposed_cardinal_module_regulation.resize( max_modules );
+
+      std::copy(
+        std::begin( exposed_cardinal_module_regulation ),
+        std::end( exposed_cardinal_module_regulation ),
         std::back_inserter( res )
       );
 
     } }
   );
 
-  return hola::matrix_from_array( res.data(), num_live_cardinals, max_modules );
+  return hola::matrix_from_array(
+    res.data(), num_live_cardinals, max_regulation_dims
+  );
 
 }
 
