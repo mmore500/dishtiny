f34f73fb5f3220db583a21b224bffa3c5cbd8554
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-02 18:43:54 -0500

    Pass const char*, not std::string, to std::system

--- include/dish2/record/global_records_initialize.hpp
+++ include/dish2/record/global_records_initialize.hpp
@@ -8,19 +8,21 @@
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 #include "../../../third-party/Empirical/include/emp/base/assert.hpp"
+#include "../../../third-party/Empirical/include/emp/tools/string_utils.hpp"
 
 namespace dish2 {
 
 void global_records_initialize() {
 
   const std::string path{ "./drawings" };
+  const std::string command{ emp::to_string( "mkdir -p ", path ) };
   if ( uitsl::is_root() && !std::filesystem::exists( path ) ) {
 
     // use mkdir as a fallback
     const bool res = std::filesystem::create_directories( path )
       || std::filesystem::create_directory( path )
       || mkdir( path.c_str(), 0777 ) == 0
-      || std::system(emp::to_string( "mkdir -p ", path )) == 0;
+      || std::system( command.c_str() ) == 0;
 
     emp_always_assert(
       res && std::filesystem::exists( path ),
