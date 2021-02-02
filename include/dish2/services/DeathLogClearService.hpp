bc377c404a61c20ecad64e1a002e6e4417994e4e
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-01 22:06:53 -0500

    Bugfix: clear death log for dead cells too

--- include/dish2/services/DeathLogClearService.hpp
+++ include/dish2/services/DeathLogClearService.hpp
@@ -20,8 +20,7 @@ struct DeathLogClearService {
   static bool ShouldRun( const size_t update, const bool alive ) {
     const size_t freq = dish2::cfg.DEATH_LOG_CLEAR_SERVICE_FREQUENCY();
     return
-      alive
-      && freq > 0
+      freq > 0
       && uitsl::shift_mod( update, freq ) == 0;
   }
 
