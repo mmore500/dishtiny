87f80bddcc856a2babfa326a544a06069c3b60d5
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-02 15:40:13 -0500

    Beef up mkdir call

--- include/dish2/record/global_records_initialize.hpp
+++ include/dish2/record/global_records_initialize.hpp
@@ -5,8 +5,6 @@
 #include <cstdlib>
 #include <string>
 
-#include <sys/stat.h>
-
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 #include "../../../third-party/Empirical/include/emp/base/assert.hpp"
@@ -17,10 +15,16 @@ void global_records_initialize() {
 
   const std::string path{ "./drawings" };
   if ( uitsl::is_root() && !std::filesystem::exists( path ) ) {
+
     // use mkdir as a fallback
-    emp_always_assert(
-      std::filesystem::create_directories( path )
+    const bool res = std::filesystem::create_directories( path )
+      || std::filesystem::create_directory( path )
       || mkdir( path.c_str(), 0777 ) == 0
+      || std::system(emp::to_string( "mkdir -p ", path )) == 0;
+
+    emp_always_assert(
+      res && std::filesystem::exists( path ),
+      res, std::filesystem::exists( path )
     );
   }
 
