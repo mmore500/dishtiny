5f6b39661b92d621b07b3f9c34ab474ee78f8344
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-08 12:19:03 -0500

    Add support for binary, gzipped innoculums

--- include/dish2/load/load_innoculum.hpp
+++ include/dish2/load/load_innoculum.hpp
@@ -5,12 +5,14 @@
 #include <algorithm>
 #include <fstream>
 
+#include "../../../third-party/cereal/include/cereal/archives/binary.hpp"
 #include "../../../third-party/cereal/include/cereal/archives/json.hpp"
 #include "../../../third-party/conduit/include/uitsl/math/math_utils.hpp"
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 #include "../../../third-party/Empirical/include/emp/base/always_assert.hpp"
 #include "../../../third-party/Empirical/include/emp/tools/keyname_utils.hpp"
+#include "../../../third-party/header-only-gzstream/include/hogzstr/gzstream.hpp"
 
 #include "../genome/Genome.hpp"
 
@@ -23,11 +25,24 @@ dish2::Genome<Spec> load_innoculum(
 
   dish2::Genome<Spec> innoculum;
 
-  {
+  if (!emp::keyname::unpack(path).count("ext")) emp_always_assert(false, path);
+  else if ( emp::keyname::unpack( path ).at("ext") == ".json" ) {
     std::ifstream fs( path );
     cereal::JSONInputArchive iarchive( fs );
     iarchive( innoculum );
-  }
+  } else if ( emp::keyname::unpack( path ).at("ext") == ".json.gz" ) {
+    hogzstr::igzstream fs( path );
+    cereal::JSONInputArchive iarchive( fs );
+    iarchive( innoculum );
+  } else if ( emp::keyname::unpack( path ).at("ext") == ".bin" ) {
+    std::ifstream fs( path );
+    cereal::BinaryInputArchive iarchive( fs );
+    iarchive( innoculum );
+  } else if ( emp::keyname::unpack( path ).at("ext") == ".bin.gz" ) {
+    hogzstr::igzstream fs( path );
+    cereal::BinaryInputArchive iarchive( fs );
+    iarchive( innoculum );
+  } else emp_always_assert(false, path, emp::keyname::unpack( path ).at("ext"));
 
   // all innoculums must specify root id
   emp_always_assert(emp::keyname::unpack( path ).count("root_id"), path);
