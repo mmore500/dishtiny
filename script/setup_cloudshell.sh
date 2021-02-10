2754a4df388f102df747e53f1cb0fd5afcc6b208
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-10 00:23:23 -0500

    Try breaking up multiline bash command

--- script/setup_cloudshell.sh
+++ script/setup_cloudshell.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 mkdir -p ~/.cloudshell/ && touch ~/.cloudshell/no-apt-get-warning
-sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list && \
-    sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9 && \
-    sudo apt-get update
+sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+sudo apt-get update
 sudo apt-get install -y popularity-contest
 sudo apt-get install -y docker singularity-container xvfb
+
 export DISPLAY=":$$"
 rm -f "/tmp/.X$$-lock"
 Xvfb "${DISPLAY}" -auth /dev/null/ &
