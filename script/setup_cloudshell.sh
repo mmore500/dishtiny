e9bbe0cca019fb3e217db66194274a9b82da205a
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-10 00:40:48 -0500

    Disable stdin for all apt commands

--- script/setup_cloudshell.sh
+++ script/setup_cloudshell.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir -p ~/.cloudshell/ && touch ~/.cloudshell/no-apt-get-warning
-sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
-sudo apt-get update
+: | sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+: | sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+: | sudo apt-get update
 : | sudo apt-get install -y docker singularity-container xvfb
 
 export DISPLAY=":$$"
