75b71854bc52da19a872ee1600c56f4c940246b8
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-11 17:29:49 -0500

    Fixup longlink test suite, patch edge cases

--- include/dish2/utility/pare_keyname_filename.hpp
+++ include/dish2/utility/pare_keyname_filename.hpp
@@ -107,17 +107,40 @@ std::string longlink_entire_filename(
 
 } // namespace internal
 
+std::filesystem::path make_longlinked_metadata_path( std::filesystem::path p ) {
+
+  p += internal::longlink_suffix;
+
+  emp_assert(
+    uitsl::safe_leq(
+      p.filename().string().size(),
+      pathconf(std::filesystem::absolute(p).parent_path().c_str(), _PC_NAME_MAX)
+    ),
+    p.filename(), p.filename().string().size(),
+    p, std::filesystem::absolute(p).parent_path(),
+    pathconf( std::filesystem::absolute(p).parent_path().c_str(), _PC_NAME_MAX )
+  );
+
+  return p;
+
+}
+
+
 std::string pare_keyname_filename(
   const std::string& original_filename, const std::filesystem::path& path="./"
 ) {
 
+  emp_assert( std::filesystem::exists( path ) );
+
   const size_t filename_max = pathconf( path.c_str(), _PC_NAME_MAX );
+  const size_t longlinked_filename_max
+    = internal::get_longlinked_filename_max( path );
 
   if ( original_filename.size() <= filename_max ) return original_filename;
 
   std::string filename( original_filename );
 
-  while ( filename.size() > internal::get_longlinked_filename_max( path ) ) {
+  while ( filename.size() > longlinked_filename_max ) {
 
     const auto res = internal::longlink_longest_value( filename );
 
@@ -132,9 +155,10 @@ std::string pare_keyname_filename(
     << " -> " << filename << std::endl;
 
   // create additional suffixed-file at path/ containing original filename
-  const auto meta_filename = filename + internal::longlink_suffix;
-  emp_assert( meta_filename.size() <= filename_max );
-  std::ofstream( path / meta_filename ) << original_filename << std::endl;
+  const auto meta_path = dish2::make_longlinked_metadata_path(path / filename);
+  std::ofstream( meta_path ) << original_filename << std::endl;
+
+  emp_assert( std::filesystem::exists( meta_path ) );
 
   // return shortened filename
   emp_assert( filename.size() <= filename_max );
