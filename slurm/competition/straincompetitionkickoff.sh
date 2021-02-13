c0992f09a3dae400536c02d6ff38d5d3ead4b183
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-02-13 12:36:37 -0500

    Organize data by processing stage

--- slurm/competition/straincompetitionkickoff.sh
+++ slurm/competition/straincompetitionkickoff.sh
@@ -84,8 +84,8 @@ python3 "dishtiny/script/layout_interstrain_tournament.py" "\${TOURNAMENT_LAYOUT
 
   read -r FIRST_COMPETITOR_SERIES SECOND_COMPETITOR_SERIES <<<"\${LINE}"
 
-  FIRST_COMPETITOR="s3://dnh2v/endeavor=\${ENDEAVOR}/genomes/stint=${STINT}/series=\${FIRST_COMPETITOR_SERIES}/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${FIRST_COMPETITOR_SERIES}+stint=${STINT}+thread=0+ext=.json.gz"
-  SECOND_COMPETITOR="s3://dnh2v/endeavor=\${ENDEAVOR}/genomes/stint=${STINT}/series=\${SECOND_COMPETITOR_SERIES}/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${SECOND_COMPETITOR_SERIES}+stint=${STINT}+thread=0+ext=.json.gz"
+  FIRST_COMPETITOR="s3://dnh2v/endeavor=\${ENDEAVOR}/genomes/stint=${STINT}/series=\${FIRST_COMPETITOR_SERIES}/stage=0+what=generated/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${FIRST_COMPETITOR_SERIES}+stint=${STINT}+thread=0+ext=.json.gz"
+  SECOND_COMPETITOR="s3://dnh2v/endeavor=\${ENDEAVOR}/genomes/stint=${STINT}/series=\${SECOND_COMPETITOR_SERIES}/stage=0+what=generated/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${SECOND_COMPETITOR_SERIES}+stint=${STINT}+thread=0+ext=.json.gz"
 
   echo "FIRST_COMPETITOR \${FIRST_COMPETITOR}"
   echo "SECOND_COMPETITOR \${SECOND_COMPETITOR}"
@@ -95,7 +95,7 @@ container_tag: ${CONTAINER_TAG}
 repo_sha: ${REPO_SHA}
 first_competitor_url: "\${FIRST_COMPETITOR}"
 second_competitor_url: "\${SECOND_COMPETITOR}"
-output_url: "s3://dnh2v/endeavor=\${ENDEAVOR}/strain-competitions/stint=${STINT}/"
+output_url: "s3://dnh2v/endeavor=\${ENDEAVOR}/strain-competitions/stint=${STINT}/stage=1+what=generated/"
 replicate: 0
 endeavor: "\${ENDEAVOR}"
 stint: "${STINT}"
