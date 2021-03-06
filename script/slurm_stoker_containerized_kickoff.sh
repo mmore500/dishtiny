4c0d80f85f61d02e94dfc1e5b28a997984edcaed
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-06 18:34:33 -0500

    Improve logging

--- script/slurm_stoker_containerized_kickoff.sh
+++ script/slurm_stoker_containerized_kickoff.sh
@@ -82,7 +82,12 @@ echo "--------------------------------"
 
 # adapted from https://superuser.com/a/689340
 # and https://stackoverflow.com/a/4642975
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 10 -qO- "https://raw.githubusercontent.com/mmore500/dishtiny/${repo_sha}/script/host_sbatch.sh" | bash -s "${JOB_SCRIPT}"
+host_sbatch_url="https://raw.githubusercontent.com/mmore500/dishtiny/${ repo_sha}/script/host_sbatch.sh"
+echo "host_sbatch_url ${host_sbatch_url}"
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 10 -qO- "${host_sbatch_url}" \
+  | bash -s "${JOB_SCRIPT}" \
+  && rm -f "${JOB_SCRIPT}" \
+  && echo "${JOB_SCRIPT} submitted"
 
 
 ################################################################################
