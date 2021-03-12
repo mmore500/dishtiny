a82067d4de75fc060365813600f49c4b73b25d08
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-11 22:50:36 -0500

    Phen diff is deterministic, don't need replicates

--- slurm/phenotype-differentiation/mutantdeletionphenotype-differentiationkickoff.sh
+++ slurm/phenotype-differentiation/mutantdeletionphenotype-differentiationkickoff.sh
@@ -88,7 +88,7 @@ echo "------------------------------"
 ################################################################################
 
 for JUST_ONE_SERIES in ${SERIES}; do
-  for REPLICATE in {0..19}; do
+  for REPLICATE in 0; do
 
     FIRST_SUBJECT="s3://${BUCKET}/endeavor=\${ENDEAVOR}/genomes/stage=0+what=generated/stint=${STINT}/series=\${JUST_ONE_SERIES}/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${JUST_ONE_SERIES}+stint=${STINT}+thread=0+variation=master+ext=.json.gz"
     SECOND_SUBJECT="s3://${BUCKET}/endeavor=\${ENDEAVOR}/genomes/stage=0+what=generated/stint=${STINT}/series=\${JUST_ONE_SERIES}/a=genome+criteria=abundance+morph=wildtype+proc=0+series=\${JUST_ONE_SERIES}+stint=${STINT}+thread=0+variation=master+ext=.json.gz"
