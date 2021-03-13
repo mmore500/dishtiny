d05d4df124a30db60ac60fc0dbcb322d2a9abb4b
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-12 23:55:28 -0500

    Add script to purge hpcc logs

---
+++ script/purge_logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+################################################################################
+echo
+echo "running purge_logs.sh"
+echo "---------------------"
+################################################################################
+
+if (( "$#" != 1 )); then
+  echo "USAGE: [num_days_to_keep]"
+  exit 1
+fi
+
+num_days="${1}"
+echo "num_days ${num_days}"
+shift
+
+find /mnt/home/$(whoami)/slurmlogs -mtime "+5${num_days}" -exec rm {} \;
+find /mnt/home/$(whoami)/slurmscripts -mtime "+${num_days}" -exec rm {} \;
+find /mnt/scratch/$(whoami)/slurmlogs -mtime "+5${num_days}" -exec rm {} \;
+find /mnt/scratch/$(whoami)/slurmscripts -mtime "+${num_days}" -exec rm {} \;
