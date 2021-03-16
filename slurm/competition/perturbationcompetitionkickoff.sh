8ae616b06cd7c3d5e14789a8be17918b6d27da3e
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-16 18:32:09 -0400

    Add logging

--- slurm/competition/perturbationcompetitionkickoff.sh
+++ slurm/competition/perturbationcompetitionkickoff.sh
@@ -118,6 +118,8 @@ for JUST_ONE_TARGET_IDX in $(eval echo ${TARGET_IDX})""; do
 
       echo "FIRST_COMPETITOR \${FIRST_COMPETITOR}"
       echo "SECOND_COMPETITOR \${SECOND_COMPETITOR}"
+      echo "SERIES \${SERIES}"
+      echo "TARGET_IDX \${TARGET_IDX}"
 
       j2 --format=yaml -o "a=competition+series=\${JUST_ONE_SERIES}+stint=${STINT}+target_idx=\${JUST_ONE_TARGET_IDX}+replicate=\${REPLICATE}+ext=.slurm.sh" "dishtiny/slurm/competition/competitionjob.slurm.sh.jinja" << J2_HEREDOC_EOF
 bucket: ${BUCKET}
