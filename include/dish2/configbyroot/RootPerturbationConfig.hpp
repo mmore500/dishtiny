bccb2d8476b4478fc14f85ab05ca22d28ba94e6e
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-19 21:54:17 -0400

    Fix typo in output column names

--- include/dish2/configbyroot/RootPerturbationConfig.hpp
+++ include/dish2/configbyroot/RootPerturbationConfig.hpp
@@ -109,13 +109,13 @@ struct RootPerturbationConfig {
         emp::to_string(intermittent_writable_state_rotate_probability)},
       {"Writable State Target Idx",
         emp::to_string(writable_state_target_idx)},
-      {"Inter Message Self-Send Fitler Mod",
+      {"Inter Message Self-Send Filter Mod",
         emp::to_string(inter_message_selfsend_filter_mod)},
-      {"Inter Message Self-Send Fitler Target",
+      {"Inter Message Self-Send Filter Target",
         emp::to_string(inter_message_selfsend_filter_target)},
-      {"Intra Message Self-Send Fitler Mod",
+      {"Intra Message Self-Send Filter Mod",
         emp::to_string(intra_message_selfsend_filter_mod)},
-      {"Intra Message Self-Send Fitler Target",
+      {"Intra Message Self-Send Filter Target",
         emp::to_string(intra_message_selfsend_filter_target)},
     };
   }
