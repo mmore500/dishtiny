5a0d8ba690989de234eee441528b322fa8d03617
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-03-25 18:58:38 -0400

    Handle saving multiple drawings
    As specified by config parameter

--- include/dish2/record/write_drawings.hpp
+++ include/dish2/record/write_drawings.hpp
@@ -4,6 +4,8 @@
 
 #include <mutex>
 
+#include "../../../third-party/Empirical/include/emp/tools/string_utils.hpp"
+
 #include "../world/ThreadWorld.hpp"
 
 #include "drawings/DrawerCollection.hpp"
@@ -14,9 +16,23 @@ template< typename Spec >
 void write_drawings(
   const dish2::ThreadWorld< Spec >& thread_world, const size_t thread_idx
 ) {
+  using polymorphic_drawers_t = typename dish2::DrawerCollection<Spec>::polymorphic_drawers_t;
+  // setup drawers as passed in args
+  thread_local emp::vector<polymorphic_drawers_t> drawers;
 
-  dish2::DrawerCollection<Spec> drawers( thread_world, thread_idx );
-  drawers.SaveToFile();
+  thread_local std::once_flag make_drawers_once;
+  std::call_once(make_drawers_once, [thread_world, thread_idx](){
+    emp::vector<std::string> drawing_names;
+    emp::slice(cfg.DRAWINGS(), drawing_names, ':');
+
+    dish2::DrawerCollection<Spec> collection( thread_world, thread_idx );
+
+    drawers = collection.MakePolymorphicDrawers(drawing_names);
+  });
+
+  for (auto drawer : drawers) {
+    drawer.SaveToFile();
+  }
 
   thread_local std::once_flag once_flag;
   std::call_once(once_flag, [thread_idx](){
