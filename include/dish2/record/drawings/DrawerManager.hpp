df841ff2dfa82f5dd3aa98c7f81d37622e997d2c
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-03-25 18:56:29 -0400

    Use PolymorphicDrawer in DrawerManager

--- include/dish2/record/drawings/DrawerManager.hpp
+++ include/dish2/record/drawings/DrawerManager.hpp
@@ -58,10 +58,24 @@ struct DrawerManager<FirstDrawer, SubsequentDrawers...> {
   FirstDrawer first_drawer;
   DrawerManager<SubsequentDrawers...> subsequent_drawers;
 
+  using polymorphic_drawers_t = PolymorphicDrawer< FirstDrawer, SubsequentDrawers...>;
+
+  constexpr static size_t num_drawers = sizeof...(SubsequentDrawers) + 1;
+
+  emp::array<polymorphic_drawers_t, num_drawers> result_table;
+
+  DrawerManager() = delete;
+
+  DrawerManager(const DrawerManager&) = default;
+
   template<typename... Args>
   DrawerManager( Args&&... args )
   : first_drawer( std::forward<Args>( args )... )
   , subsequent_drawers( std::forward<Args>( args )... )
+  , result_table{
+      polymorphic_drawers_t{ std::in_place_type_t<FirstDrawer>{}, std::forward<Args>( args )... },
+      polymorphic_drawers_t{ std::in_place_type_t<SubsequentDrawers>{}, std::forward<Args>( args )... }...
+    }
   {}
 
   void SaveToFile() {
@@ -70,7 +84,43 @@ struct DrawerManager<FirstDrawer, SubsequentDrawers...> {
     worker.join();
   }
 
+  emp::vector<polymorphic_drawers_t>
+  MakePolymorphicDrawers(const emp::vector<std::string>& names) {
+    static constexpr std::string_view lookup[]
+      = { FirstDrawer::artist_t::GetName(), SubsequentDrawers::artist_t::GetName()... };
+
+    emp::vector<polymorphic_drawers_t> res;
+
+    std::transform(
+      names.begin(),
+      names.end(),
+      std::back_inserter(res),
+      [this](const auto& name) {
+        const auto found = std::find_if(
+          std::begin( lookup ),
+          std::end( lookup ),
+          [&name](const auto a) {
+            return emp::slugify(std::string{a}) == emp::slugify(name);
+          }
+        );
+
+        std::cout << "created drawer for " << emp::slugify(name) << std::endl;
+
+        emp_always_assert(
+          found != std::end( lookup ), emp::slugify(name)
+        );
+
+        const size_t idx = std::distance(
+          std::begin( lookup ),
+          found
+        );
+        return result_table[idx];
+      }
+    );
+    return res;
+  }
 };
+
 
 } // namespace dish2
 
