eeea3f7fcbd4b808b1ea303760fb61da6928e41f
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-03-27 23:37:28 -0400

    Fix copy/paste bug

--- include/dish2/configbyroot/RootPerturbationConfig.hpp
+++ include/dish2/configbyroot/RootPerturbationConfig.hpp
@@ -92,11 +92,11 @@ struct RootPerturbationConfig {
   std::map<std::string, std::string> MakeSummary() const {
     return {
       {"Intermittent Extrospective State Exchange Probability",
-        emp::to_string(intermittent_introspective_state_exchange_probability)},
+        emp::to_string(intermittent_extrospective_state_exchange_probability)},
       {"Intermittent Extrospective State Rotate Probability",
-        emp::to_string(intermittent_introspective_state_rotate_probability)},
+        emp::to_string(intermittent_extrospective_state_rotate_probability)},
       {"Extrospective State Target Idx",
-        emp::to_string(introspective_state_target_idx)},
+        emp::to_string(extrospective_state_target_idx)},
       {"Intermittent Introspective State Exchange Probability",
         emp::to_string(intermittent_introspective_state_exchange_probability)},
       {"Intermittent Introspective State Rotate Probability",
