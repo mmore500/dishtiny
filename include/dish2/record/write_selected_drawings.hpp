2a6fa4f380d2bf5b0b5b12f5a0c6496e0c610f30
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-01 15:28:59 -0400

    Refactor away thread_local flag

--- include/dish2/record/write_selected_drawings.hpp
+++ include/dish2/record/write_selected_drawings.hpp
@@ -21,18 +21,16 @@ void write_selected_drawings( const dish2::ThreadWorld< Spec >& thread_world ) {
 
   using polymorphic_drawers_t = typename dish2::DrawerCollection<Spec>::polymorphic_drawers_t;
 
-  thread_local emp::vector<polymorphic_drawers_t> drawers;
+  thread_local emp::vector<polymorphic_drawers_t> drawers = [&thread_world](){
 
-  // setup drawers as passed in args
-  thread_local std::once_flag make_drawers_once;
-  std::call_once(make_drawers_once, [&thread_world](){
     emp::vector<std::string> drawing_names;
     emp::slice(cfg.SELECTED_DRAWINGS(), drawing_names, ':');
 
     dish2::DrawerCollection<Spec> collection( thread_world );
 
-    drawers = collection.MakePolymorphicDrawers(drawing_names);
-  });
+    return collection.MakePolymorphicDrawers(drawing_names);
+
+  }();
 
   const auto parent_thread_idx = dish2::thread_idx;
 
