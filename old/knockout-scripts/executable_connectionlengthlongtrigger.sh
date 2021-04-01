889c5799fec0a991442ccfc7546d6c13b5bd2993
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-01 14:39:05 -0400

    Delete outdated content

--- old/knockout-scripts/executable_connectionlengthlongtrigger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# sed magic adapted from https://unix.stackexchange.com/a/200576
-
-KO_PATH=$1
-
-touch "${KO_PATH}___knockout_changelog+ext=.txt"
--- old/mono-local/instantiate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for treat in $(ls mono-local/*.tar.gz | grep -o 'batch.*+' | sed 's/.$//'); do echo $treat; export treat=$treat; j2 "mono-local/run_list.slurm.sh.jinja" -o "mono-local/instantiations/treat=${treat}+ext=.slurm.sh"; done
