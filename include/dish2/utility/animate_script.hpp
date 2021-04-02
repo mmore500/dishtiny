e70abf0a133f6ceaa7dcc1254f1c4659a93a05bf
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-01 22:49:30 -0400

    Fix whitespace, add comments and asserts

--- include/dish2/utility/animate_script.hpp
+++ include/dish2/utility/animate_script.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #ifndef DISH2_UTILITY_ANIMATE_SCRIPT_HPP_INCLUDE
 #define DISH2_UTILITY_ANIMATE_SCRIPT_HPP_INCLUDE
+
 #include <filesystem>
 #include <fstream>
 #include <string>
 
 #include "../../../third-party/conduit/include/uitsl/fetch/make_temp_filepath.hpp"
+#include "../../../third-party/Empirical/include/emp/base/assert.hpp"
 
 namespace dish2::py {
 
@@ -15,14 +17,23 @@ const std::string script_animate_frames{
 
 };
 
-    std::filesystem::path setup_script_animate() {
-        const auto path = uitsl::make_temp_filepath();
-        std::ofstream ofs(path);
+std::filesystem::path setup_script_animate() {
 
-        ofs << script_animate_frames.substr(1, script_animate_frames.size() - 2);
+  const auto path = uitsl::make_temp_filepath();
+  std::ofstream ofs(path);
 
-        return path;
-    }
+  emp_assert( script_animate_frames.front() == '"' );
+  emp_assert( script_animate_frames.back() == '"' );
+
+  // strip leading and trailing aftifact "'s from included string
+  ofs << script_animate_frames.substr(
+    1, script_animate_frames.size() - 2
+  );
+
+  return path;
+
+}
+
 }
 
 #endif // #ifndef DISH2_UTILITY_ANIMATE_SCRIPT_HPP_INCLUDE
