9a227d56d9da2df8cbe4b78b2c286e96556c8fa6
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-03 14:00:30 -0400

    Move polymorphic drawer to own file

--- include/dish2/record/drawings/DrawerManager.hpp
+++ include/dish2/record/drawings/DrawerManager.hpp
@@ -7,48 +7,15 @@
 #include <string>
 #include <thread>
 #include <utility>
-#include <variant>
 
 #include "../../../../third-party/Empirical/include/emp/base/array.hpp"
 #include "../../../../third-party/Empirical/include/emp/base/vector.hpp"
 
 #include "../../config/thread_idx.hpp"
 
+#include "PolymorphicDrawer.hpp"
+
 namespace dish2 {
-
-template< typename... Drawers >
-class PolymorphicDrawer {
-
-    std::variant<Drawers...> impl;
-
-public:
-    PolymorphicDrawer() = delete;
-
-    PolymorphicDrawer(const PolymorphicDrawer&) = default;
-
-    PolymorphicDrawer& operator=(const PolymorphicDrawer&) = default;
-
-    template<typename Drawer, typename... Args>
-    PolymorphicDrawer( std::in_place_type_t<Drawer> tag, Args&&... args )
-    : impl( tag, std::forward<Args>( args )... )
-    { }
-
-    void SaveToFileAsDrawing() {
-        // call Draw on the variant impl
-        std::visit(
-            [&](auto& drawer){ drawer.SaveToFileAsDrawing(); },
-            impl
-        );
-    }
-
-    void SaveToFileAsFrame() {
-        // call Draw on the variant impl
-        std::visit(
-            [&](auto& drawer){ drawer.SaveToFileAsFrame(); },
-            impl
-        );
-    }
-};
 
 // base case
 template<typename... SubsequentDrawers> struct DrawerManager {
