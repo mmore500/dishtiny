22522ff5d9eb1cd1df493c7e0e247bbd1a3b195f
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-04-04 13:11:54 -0400

    Update peripheral tests

--- tests/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/IsAlive.cpp
+++ tests/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/IsAlive.cpp
@@ -2,7 +2,7 @@
 
 #include "Catch/single_include/catch2/catch.hpp"
 
-#include "dish2/peripheral/readable_state/introspective_state/IsAlive.hpp"
+#include "dish2/peripheral/readable_state/introspective_state/raw_introspective_state/IsAlive.hpp"
 
 TEST_CASE("Test IsAlive") {
   dish2::IsAlive{};
--- tests/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/ResourceStockpile.cpp
+++ tests/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/ResourceStockpile.cpp
@@ -2,7 +2,7 @@
 
 #include "Catch/single_include/catch2/catch.hpp"
 
-#include "dish2/peripheral/readable_state/introspective_state/ResourceStockpile.hpp"
+#include "dish2/peripheral/readable_state/introspective_state/raw_introspective_state/ResourceStockpile.hpp"
 
 TEST_CASE("Test ResourceStockpile") {
   dish2::ResourceStockpile{};
