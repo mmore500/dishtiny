fc8090758c7179a9abd816cf6ecf8dc6f0b7ddc5
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-08 23:19:06 -0400

    Run tests with python3.8 or better

--- dishpylib/test/run_tests.sh
+++ dishpylib/test/run_tests.sh
@@ -4,4 +4,13 @@ set -e # exit with error if any of this fails
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-nosetests "${script_dir}"/**/test_*.py
+which_python="$(\
+  command -v python3.8 \
+  || command -v python3.9 \
+  || echo python3 \
+)"
+echo "which_python ${which_python}"
+
+"${which_python}" -c "import sys; assert sys.version_info >= (3, 8, 0)"
+
+"${which_python}" "$(which nosetests)" "${script_dir}"/**/test_*.py
