0433a742cee75dac5f0a9e06b3b16f88e3bb857b
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-10 16:19:47 -0400

    Move tee_plot to PyPi package

--- dishpylib/pyanalysis/__init__.py
+++ dishpylib/pyanalysis/__init__.py
@@ -1,4 +1,3 @@
 from .count_hands_with_k_or_more_sets import count_hands_with_k_or_more_sets
 from .count_hands_without_k_or_more_sets \
     import count_hands_without_k_or_more_sets
-from .tee_plot import tee_plot
