c06ff0c8a1947652ad84539bde65270090190d71
mmore500 <mmore500.login@gmail.com>
2021-05-12 04:43:35 +0000

    Deploying to binder from @ mmore500/dishtiny@ed6c0adfcc84bd8ad29b1af11d96cfb240bddf1f 🚀

---
+++ dishpylib/pyanalysis/calc_loglikelihoods_by_num_sets.py
@@ -0,0 +1,116 @@
+import math
+import numpy as np
+
+from .count_hands_with_k_or_more_sets import count_hands_with_k_or_more_sets
+from .count_hands_without_k_or_more_sets import count_hands_without_k_or_more_sets
+
+def _count_nop_targetings_consistent_with_observation(
+        *,
+        num_nopped,
+        num_sets,
+        num_sites,
+        set_size,
+        was_significantly_divergent,
+    ):
+
+    res = (
+        count_hands_with_k_or_more_sets
+        if was_significantly_divergent
+        else count_hands_without_k_or_more_sets
+    )(
+        deck_size=num_sites,
+        hand_size=num_nopped,
+        num_sets=num_sets,
+        set_size=set_size,
+        k=1,
+    )
+
+    assert res >= 0
+    assert res <= math.comb(num_sites, num_nopped)
+
+    return res
+
+
+def _calc_loglikelihood_of_observations(
+        *,
+        interpolation_competitions_df,
+        num_sets,
+        series,
+        set_size,
+    ):
+
+    # filter for just this series,
+    # from the point of view of the nop-interpolated variants
+    # not the controls which have nans for num nopped
+    fil = interpolation_competitions_df[
+        interpolation_competitions_df['genome series'] == series
+    ].dropna(
+        subset=['genome nop_interpolation_num_nopped']
+    )
+    num_sites = int(fil['genome nop_interpolation_num_nopped'].max())
+
+    # sum logp of observations assuming num_sets, set_size
+    total_logp = 0.0
+    for idx, row in fil.iterrows():
+
+        num_nopped = int(row['genome nop_interpolation_num_nopped'])
+
+        res = _count_nop_targetings_consistent_with_observation(
+            num_nopped=num_nopped,
+            num_sets=num_sets,
+            num_sites=num_sites,
+            set_size=set_size,
+            was_significantly_divergent=(
+                row['Is Less Fit'] or row['Is More Fit']
+            ),
+        )
+
+        if res:
+            # log-land equivalent of
+            # *= num_nop_targetings_for_obvs / total_num_nop_targetings
+            total_logp += (
+                math.log(res)
+                - math.log(math.comb(num_sites, num_nopped))
+            )
+        else:
+            # if we have a zero-probabilty observation, bail and return nan
+            return math.nan
+
+    return total_logp
+
+
+def calc_loglikelihoods_by_num_sets(
+        *,
+        interpolation_competitions_df,
+        series,
+        set_size,
+    ):
+
+    num_sites = int(max(filter(
+        np.isfinite,
+        interpolation_competitions_df[
+            interpolation_competitions_df['genome series'] == series
+        ]['genome nop_interpolation_num_nopped'],
+    )))
+    max_num_sets = num_sites // set_size
+
+    loglikelihoods = []
+    for num_sets in range(max_num_sets):
+
+        cur = _calc_loglikelihood_of_observations(
+            interpolation_competitions_df=interpolation_competitions_df,
+            num_sets=num_sets,
+            series=series,
+            set_size=set_size,
+        )
+
+        loglikelihoods.append( cur )
+
+        # if 1000x less likely than best num_sets seen so far,
+        # larger num_sets won't be worth investigating so bail early
+        # (assumes that distribution of loglikelihoods is unimodal)
+        if any(map(np.isfinite, loglikelihoods)) \
+            and cur < max(filter(np.isfinite, loglikelihoods)) - np.log(1000):
+            break
+
+    return loglikelihoods
---
+++ dishpylib/pyanalysis/estimate_interpolation_complexity.py
@@ -0,0 +1,81 @@
+from iterpop import iterpop as ip
+import itertools as it
+import math
+import numpy as np
+
+from .calc_loglikelihoods_by_num_sets import calc_loglikelihoods_by_num_sets
+from .estimate_credible_interval import estimate_credible_interval
+
+def estimate_interpolation_complexity(
+        *,
+        series,
+        interpolation_competitions_df,
+    ):
+
+    # filter for just this series,
+    # from the point of view of the nop-interpolated variants
+    # not the controls which have nans for num nopped
+    fil = interpolation_competitions_df[
+        interpolation_competitions_df['genome series'] == series
+    ].dropna(
+        subset=['genome nop_interpolation_num_nopped'],
+    )
+
+    res = {
+        'none_nopped_is_less_fit' : (
+            ip.popsingleton(fil[
+                fil['genome nop_interpolation_num_nopped'] == 0
+            ]['Is Less Fit'])
+        ),
+        'all_nopped_is_less_fit' : (
+            ip.popsingleton(fil[
+                fil['genome nop_interpolation_num_nopped']
+                == max(fil['genome nop_interpolation_num_nopped'])
+            ]['Is Less Fit'])
+        ),
+        'credible_interval_lower_bound_idx' : math.nan,
+        'most_credible_idx' : math.nan,
+        'most_credible_likelihood' : math.nan,
+        'most_credible_set_size' : math.nan,
+        'interpolation_complexity' : math.nan,
+        'credible_interval_upper_bound_idx' : math.nan,
+    }
+
+    if res['none_nopped_is_less_fit'] or not res['all_nopped_is_less_fit']:
+        return res
+
+    for set_size in it.count(start=1):
+        loglikelihoods = calc_loglikelihoods_by_num_sets(
+            interpolation_competitions_df=interpolation_competitions_df,
+            series=series,
+            set_size=set_size,
+        )
+        likelihoods = [np.exp(loglike) for loglike in loglikelihoods]
+
+        cur = estimate_credible_interval(likelihoods)
+
+        res[f'a=most_credible_likelihood+set_size={set_size}'] \
+            = cur['most_credible_likelihood']
+        res[f'a=most_credible_idx+set_size={set_size}'] \
+            = cur['most_credible_idx']
+
+        if math.isnan(res['most_credible_likelihood']) \
+            or cur['most_credible_likelihood'] \
+                > res['most_credible_likelihood']:
+
+            res['most_credible_set_size'] = set_size
+            # might consider multiplying this by set_size
+            res['interpolation_complexity'] = cur['most_credible_idx']
+
+            if math.isnan(cur['most_credible_likelihood']):
+                return res
+
+            # should update 'most_credible_likelihood' and other fields
+            res.update(cur)
+
+        else:
+            # likelihood was worse for this set_size
+            # don't investigate further set_sizes
+            break
+
+    return res
