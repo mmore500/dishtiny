79732bb7ea74079c860fbb2785fe772a21c8c31a
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-16 21:32:58 -0400

    Add notebooks to test_tidy.sh

---
+++ ci/test_notebooks_clear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SOURCE_HASH=$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
+
+./binder/clear_notebooks.sh
+./binder/clear_outplots.sh
+
+if [ "${SOURCE_HASH}" == "$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )" ];
+then
+  exit 0 # success
+else
+  echo "missing newline at end of file detected, run ./binder/clear_notebooks.sh ./binder/clear_outplots.sh locally to find & fix"
+  exit 1 # failure
+fi
--- ci/test_tidy.sh
+++ ci/test_tidy.sh
@@ -8,3 +8,4 @@ set -e
 ./ci/test_missing_newlines.sh && echo "✔ EOF newlines ok" || exit 1
 ./ci/test_tabs.sh && echo "✔ indentation ok" || exit 1
 ./ci/test_trailing_whitespace.sh && echo "✔ trailing whitespace ok" || exit 1
+./ci/test_notebooks_clear.sh && echo "✔ notebooks ok" || exit 1
