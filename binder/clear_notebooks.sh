64b34fa6013e02d22d4d46411068a3c27b1e78ae
mmore500 <mmore500.login@gmail.com>
2021-05-17 01:57:57 +0000

    Deploying to binder from @ mmore500/dishtiny@79732bb7ea74079c860fbb2785fe772a21c8c31a 🚀

--- binder/clear_notebooks.sh
+++ binder/clear_notebooks.sh
@@ -35,8 +35,11 @@ for notebook in "${script_dir}/"*.ipynb; do
     --clear-output \
     --inplace \
     "${notebook}"
+  # remove empty cells
+  nb-clean clean  --remove-empty-cells "${notebook}"
   # strip trailing whitespace
-  sed -i 's/\s*\\n"/\\n"/g' "${notebook}"
+  sed -i 's/\s*\\n",$/\\n",/g' "${notebook}"
+  sed -i 's/\s*"$/"/g' "${notebook}"
 done
 
 shopt -u nullglob
