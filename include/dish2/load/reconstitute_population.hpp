9c075cd4b94e12d7a37bf040df9482ae413cca8a
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-24 17:18:21 -0400

    Add kickoff for immediatepredecessor battles

--- include/dish2/load/reconstitute_population.hpp
+++ include/dish2/load/reconstitute_population.hpp
@@ -6,6 +6,7 @@
 
 #include "../../../third-party/bxzstr/include/bxzstr.hpp"
 #include "../../../third-party/cereal/include/cereal/archives/binary.hpp"
+#include "../../../third-party/conduit/include/uitsl/polyfill/erase_if.hpp"
 #include "../../../third-party/conduit/include/uitsl/polyfill/filesystem.hpp"
 #include "../../../third-party/conduit/include/uitsl/utility/keyname_directory_filter.hpp"
 #include "../../../third-party/Empirical/include/emp/base/always_assert.hpp"
@@ -45,6 +46,31 @@ void reconstitute_population( dish2::ThreadWorld<Spec>& world ) {
     "reconstituted ", reconstituted.size(), " cells from ",
     eligible_population_paths.front()
   );
+
+  const auto attrs = emp::keyname::unpack( eligible_population_paths.front() );
+
+  if ( attrs.count("filter_lowest_root") ) {
+    emp_always_assert(
+      attrs.at("filter_lowest_root") == "", attrs.at("filter_lowest_root")
+    );
+
+    const size_t lowest_root = std::min_element(
+      std::begin( reconstituted ), std::end( reconstituted ),
+      []( const auto& left_genome, const auto& right_genome ){
+        return left_genome.root_id.GetID() < right_genome.root_id.GetID();
+      }
+    )->root_id.GetID();
+    std::erase_if(
+      reconstituted,
+      [lowest_root]( const auto& genome ){
+        return genome.root_id.GetID() != lowest_root;
+      }
+    );
+    dish2::log_msg(
+      "applied lowest_root filter, ", reconstituted.size(), " cells remain "
+    );
+
+  }
 
   dish2::seed_genomes_into<Spec>( reconstituted, world );
 
