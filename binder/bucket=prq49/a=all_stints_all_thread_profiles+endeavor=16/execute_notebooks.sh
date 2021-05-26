eb1d7a7851b99bb368e4e09eadc27918f0c222c4
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-25 23:05:59 -0400

    Add notebooks for 16005 case study

---
+++ binder/bucket=prq49/a=all_stints_all_thread_profiles+endeavor=16/execute_notebooks.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+################################################################################
+echo
+echo "running execute_notebooks.sh"
+echo "---------------------------------------------"
+################################################################################
+
+# fail on error
+set -e
+
+################################################################################
+echo
+echo "other initialization"
+echo "--------------------"
+################################################################################
+
+[[ -f ~/.secrets.sh ]] && source ~/.secrets.sh || echo "~/secrets.sh not found"
+
+# adapted from https://stackoverflow.com/a/24114056
+script_dir="$(dirname -- "$BASH_SOURCE")"
+echo "script_dir ${script_dir}"
+
+################################################################################
+echo
+echo "execute notebooks in current directory"
+echo "--------------------------------------"
+################################################################################
+
+shopt -s nullglob
+
+for notebook in "${script_dir}/"*.ipynb; do
+  jupyter nbconvert \
+    --to notebook --execute --inplace \
+    --ExecutePreprocessor.timeout=600 \
+    "${notebook}"
+done
+
+shopt -u nullglob
+
+################################################################################
+echo
+echo "upload outplots in current directory"
+echo "--------------------------------------"
+################################################################################
+
+if test -d "${script_dir}/outplots/" && [[ "${1}" == "upload" ]]; then
+
+shopt -s nullglob
+cd "${script_dir}/outplots/"
+
+for outplot in *{.pdf,.png}; do
+  keyname stash --move --drop "${outplot}" _dfdigest
+done
+
+for outfile in *{.pdf,.png}*; do
+
+  bucket="$(echo "${outfile}" | keyname extract bucket)"
+  endeavor="$(echo "${outfile}" | keyname extract endeavor)"
+  echo "bucket ${bucket}"
+  echo "endeavor ${endeavor}"
+
+  aws s3 cp --quiet \
+    "${outfile}" \
+    "s3://${bucket}/endeavor=${endeavor}/stage=9+what=plotted/${outfile}" \
+    & pids+=($!)
+
+    # limit to twenty concurrent upload jobs
+    while (( $(jobs -p | wc -l) > 20 )); do sleep 1; done
+done
+
+# wait on all forked upload jobs
+for pid in "${pids[@]}"; do
+  # if child process fails, we fail
+  wait "${pid}"
+done
+
+unset pids
+
+cd -
+shopt -u nullglob
+
+fi # if test -d "${script_dir}/outplots/" && [[ "${1}" == "upload" ]]; then
+
+################################################################################
+echo
+echo "upload notebooks in current directory"
+echo "--------------------------------------"
+################################################################################
+
+if [[ "${1}" == "upload" ]]; then
+
+shopt -s nullglob
+cd "${script_dir}"
+
+bucket="$(pwd | sed 's/\//+/g' - | keyname extract bucket)"
+endeavor="$(pwd | sed 's/\//+/g' - | keyname extract endeavor)"
+echo "bucket ${bucket}"
+echo "endeavor ${endeavor}"
+
+test -n "${bucket}" && test -n "${endeavor}" && for outfile in *.ipynb; do
+
+  aws s3 cp --quiet \
+    "${outfile}" \
+    "s3://${bucket}/endeavor=${endeavor}/stage=9+what=notebooks/${outfile}" \
+    & pids+=($!)
+
+    # limit to twenty concurrent upload jobs
+    while (( $(jobs -p | wc -l) > 20 )); do sleep 1; done
+done
+
+# wait on all forked upload jobs
+for pid in "${pids[@]}"; do
+  # if child process fails, we fail
+  wait "${pid}"
+done
+
+unset pids
+
+cd -
+shopt -u nullglob
+
+fi # if test -d "${script_dir}/outplots/" && [[ "${1}" == "upload" ]]; then
+
+################################################################################
+echo
+echo "recurse to subdirectories"
+echo "-------------------------"
+################################################################################
+
+shopt -s nullglob
+
+for script in "${script_dir}/"*/execute_notebooks.sh; do
+  "${script}" "$@"
+done
+
+shopt -u nullglob
