712dcce8f74ca530978af2c8070a4b08ddde026b
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-29 21:39:13 -0400

    Log rng state
    Closes #87

--- include/dish2/debug/make_log_entry_boilerplate.hpp
+++ include/dish2/debug/make_log_entry_boilerplate.hpp
@@ -9,6 +9,7 @@
 
 #include "../../../third-party/conduit/include/uitsl/mpi/comm_utils.hpp"
 #include "../../../third-party/Empirical/include/emp/tools/string_utils.hpp"
+#include "../../../third-party/signalgp-lite/include/sgpl/utility/ThreadLocalRandom.hpp"
 
 #include "../config/thread_idx.hpp"
 #include "../utility/mkdir_exists_ok.hpp"
@@ -17,11 +18,15 @@
 namespace dish2 {
 
 std::string make_log_entry_boilerplate() {
+
+  auto dup = sgpl::tlrand.Get();
+
   return emp::to_string(
     "[proc ", uitsl::get_proc_id(), "] ",
     "[thread ", dish2::thread_idx, "] ",
     "[sysproc ", getpid(), "] ",
     "[systhread ", std::this_thread::get_id(), "] ",
+    "[rng_state ", dup.GetUInt(), "] ",
     '\n',
     "└──( ", dish2::static_coarse_timer.GetElapsed().count(), "s ) >> "
   );
