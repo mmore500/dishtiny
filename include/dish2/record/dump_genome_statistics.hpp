b8a6835ada8f3ae6ce02b8ab9f9e50769bd15867
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-05-30 01:52:25 -0400

    Fixup small bugs in data recording

--- include/dish2/record/dump_genome_statistics.hpp
+++ include/dish2/record/dump_genome_statistics.hpp
@@ -56,7 +56,8 @@ void dump_genome_statistics(
       file.AddVal( v, emp::to_string("Treatment ", k) );
     }
   }
-  file.AddVal( "proc", emp::to_string( uitsl::get_proc_id() ) );
+  file.AddVal( uitsl::get_proc_id(), "proc" );
+  file.AddVal( dish2::thread_idx, "thread" );
 
   const std::string slug = dish2::make_genome_slug( genome_filename );
   file.AddVar(slug, "Genome Slug");
