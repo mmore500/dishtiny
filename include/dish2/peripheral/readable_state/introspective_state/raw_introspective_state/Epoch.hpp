59c452d345b80bce365beb12ef187f1fd5103d3f
mmore500 <mmore500@users.noreply.github.com>
2021-06-01 04:18:46 +0000

    Deploying to binder from @ mmore500/dishtiny@01db4b60ad6b875a37ed0d9ab8a162d4b3b772b3 🚀

--- include/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/Epoch.hpp
+++ include/dish2/peripheral/readable_state/introspective_state/raw_introspective_state/Epoch.hpp
@@ -7,9 +7,9 @@
 
 namespace dish2 {
 
-struct Epoch : public uitsl::PodLeafNode<size_t> {
+struct Epoch : public uitsl::PodLeafNode<uint64_t> {
   // inherit constructors
-  using parent_t = uitsl::PodLeafNode<size_t>;
+  using parent_t = uitsl::PodLeafNode<uint64_t>;
   using parent_t::parent_t;
 };
 
