626405087c86ded41fb22649621391316d4ae2c7
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-06-23 14:31:11 -0400

    Exclude binary files from whitespace rules

--- ci/fix_missing_newlines.sh
+++ ci/fix_missing_newlines.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*.tar.gz" ! -path "*.jpg" ! -path "*.png" ! -path "*Makefile" ! -path "*Maketemplate*")
+# greq -qI tests if file is binary
+# adapted from https://stackoverflow.com/a/29689345
+TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*.tar.gz" ! -path "*.jpg" ! -path "*.png" ! -path "*Makefile" ! -path "*Maketemplate*" -exec grep -qI . {} ';' -print)
 
 for filename in $TARGETS
 do
