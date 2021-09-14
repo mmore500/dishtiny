9c6aa4298c1bc4325f4b9535f8a2124b5e3c3cbf
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-14 15:15:45 -0400

    Add and hook in script to alphabetize imports

--- ci/test_tidy.sh
+++ ci/test_tidy.sh
@@ -3,6 +3,7 @@
 set -e
 
 ./ci/test_alphabetize_includes.sh && echo "✔ include alphabetization ok" || exit 1
+./ci/test_alphabetize_imports.sh && echo "✔ import alphabetization ok" || exit 1
 ./ci/test_headerguards.sh && echo "✔ headerguards ok" || exit 1
 ./ci/test_merge_conflict_markers.sh && echo "✔ conflict markers ok" || exit 1
 ./ci/test_missing_newlines.sh && echo "✔ EOF newlines ok" || exit 1
