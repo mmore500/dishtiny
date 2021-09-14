aa47f835bf27ea9e07d3ad3f9ff01e996b1893b5
mmore500 <mmore500@users.noreply.github.com>
2021-09-14 23:51:30 +0000

    Deploying to binder from @ mmore500/dishtiny@a2241b091e603d7ecd97665563d4aff1f1bf4c06 🚀

--- dishpylib/pyassemblers/__init__.py
+++ dishpylib/pyassemblers/__init__.py
@@ -32,3 +32,41 @@ from .assemble_wildtype_doubling_time import assemble_wildtype_doubling_time
 from .either_perturbation_competitions_assembler_factory import either_perturbation_competitions_assembler_factory
 from .messaging_selfsend_competitions_assembler_factory import messaging_selfsend_competitions_assembler_factory
 from .perturbation_competitions_assembler_factory import perturbation_competitions_assembler_factory
+
+# adapted from https://stackoverflow.com/a/31079085
+__all__ = [
+    'assemble_config_records',
+    'assemble_deletion_mutant_competitions',
+    'assemble_deletion_mutant_phenotype_differentiation',
+    'assemble_evolve_dpp_metrics',
+    'assemble_evolve_dpp_metrics_threadfirst',
+    'assemble_evolve_dpp_metrics_threadmean',
+    'assemble_evolve_kin_conflict_statistics',
+    'assemble_insertion_mutant_competitions',
+    'assemble_insertion_mutant_phenotype_differentiation',
+    'assemble_lowestroot_immediatepredecessor_battles',
+    'assemble_monoculture_birth_log_statistics',
+    'assemble_monoculture_dpp_metrics_threadfirst',
+    'assemble_monoculture_dpp_metrics_threadmean',
+    'assemble_monoculture_kin_conflict_by_replev_statistics',
+    'assemble_monoculture_kin_conflict_statistics',
+    'assemble_mutant_competitions',
+    'assemble_mutant_phenotype_differentiation',
+    'assemble_mutating_competitions',
+    'assemble_noncritical_nopout_fitness_competitions',
+    'assemble_noncritical_phenotypeequivalent_nopinterpolation_competitions',
+    'assemble_phenotype_neutral_nopout_fitness_competitions',
+    'assemble_phenotype_neutral_nopout_phenotype_differentiation',
+    'assemble_phenotype_neutral_nopouts',
+    'assemble_point_mutant_competitions',
+    'assemble_point_mutant_phenotype_differentiation',
+    'assemble_predecessor_battles',
+    'assemble_predecessor_competitions',
+    'assemble_progenitor_competitions',
+    'assemble_strain_competitions',
+    'assemble_variant_competitions',
+    'assemble_wildtype_doubling_time',
+    'either_perturbation_competitions_assembler_factory',
+    'messaging_selfsend_competitions_assembler_factory',
+    'perturbation_competitions_assembler_factory',
+]
