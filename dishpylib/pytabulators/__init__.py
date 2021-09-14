f5878e49bb6612c54ca6ad5abfb763c74fa73d87
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-14 17:48:47 -0400

    Remove newline escapes in import statements

--- dishpylib/pytabulators/__init__.py
+++ dishpylib/pytabulators/__init__.py
@@ -1,5 +1,4 @@
-from .filter_for_phenotype_neutral_nopout \
-    import filter_for_phenotype_neutral_nopout
+from .filter_for_phenotype_neutral_nopout import filter_for_phenotype_neutral_nopout
 from .reshape_birth_log import reshape_birth_log
 from .reshape_dpp import reshape_dpp
 from .reshape_dpp_threadfirst import reshape_dpp_threadfirst
@@ -8,22 +7,16 @@ from .reshape_kin_conflict_by_replev import reshape_kin_conflict_by_replev
 from .reshape_kin_conflict import reshape_kin_conflict
 from .tabulate_predecessor_fitness import tabulate_predecessor_fitness
 from .tabulate_strain_fitness import tabulate_strain_fitness
-from .tabulate_predecessor_battle_fitness \
-    import tabulate_predecessor_battle_fitness
+from .tabulate_predecessor_battle_fitness import tabulate_predecessor_battle_fitness
 from .tabulate_fitness_complexity import tabulate_fitness_complexity
 from .tabulate_interpolation_complexity import tabulate_interpolation_complexity
 from .tabulate_mutant_fitness import tabulate_mutant_fitness
-from .tabulate_either_perturbation_fitness \
-    import tabulate_either_perturbation_fitness
+from .tabulate_either_perturbation_fitness import tabulate_either_perturbation_fitness
 from .tabulate_selfsend_fitness import tabulate_selfsend_fitness
-from .tabulate_phenotype_equivalent_nopout_fitness \
-    import tabulate_phenotype_equivalent_nopout_fitness
+from .tabulate_phenotype_equivalent_nopout_fitness import tabulate_phenotype_equivalent_nopout_fitness
 from .tabulate_perturbation_fitness import tabulate_perturbation_fitness
-from .tabulate_noncritical_nopout_fitness \
-    import tabulate_noncritical_nopout_fitness
+from .tabulate_noncritical_nopout_fitness import tabulate_noncritical_nopout_fitness
 from .tabulate_progenitor_fitness import tabulate_progenitor_fitness
-from .tabulate_phenotype_neutral_nopout_phenotype_differentiation \
-    import tabulate_phenotype_neutral_nopout_phenotype_differentiation
+from .tabulate_phenotype_neutral_nopout_phenotype_differentiation import tabulate_phenotype_neutral_nopout_phenotype_differentiation
 from .tabulate_wildtype_doubling_time import tabulate_wildtype_doubling_time
-from .tabulate_mutant_phenotype_differentiation \
-    import tabulate_mutant_phenotype_differentiation
+from .tabulate_mutant_phenotype_differentiation import tabulate_mutant_phenotype_differentiation
