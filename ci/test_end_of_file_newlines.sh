96fa7d182ace5ddbb1541706cdd574f1df970a64
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-26 01:38:50 -0400

    Rename {fix_missing,ensure_end_of_file}_newlines.sh

--- ci/test_end_of_file_newlines.sh
+++ ci/test_end_of_file_newlines.sh
@@ -2,12 +2,12 @@
 
 SOURCE_HASH=$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
 
-./ci/fix_missing_newlines.sh
+./ci/ensure_end_of_file_newlines.sh
 
 if [ "${SOURCE_HASH}" == "$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )" ];
 then
   exit 0 # success
 else
-  echo "missing newline at end of file detected, run ./ci/fix_missing_newlines.sh locally to find & fix"
+  echo "missing newline at end of file detected, run ./ci/ensure_end_of_file_newlines.sh locally to find & fix"
   exit 1 # failure
 fi
--- ci/test_tidy.sh
+++ ci/test_tidy.sh
@@ -6,7 +6,7 @@ set -e
 ./ci/test_alphabetize_imports.sh && echo "✔ import alphabetization ok" || exit 1
 ./ci/test_headerguards.sh && echo "✔ headerguards ok" || exit 1
 ./ci/test_merge_conflict_markers.sh && echo "✔ conflict markers ok" || exit 1
-./ci/test_missing_newlines.sh && echo "✔ EOF newlines ok" || exit 1
+./ci/test_end_of_file_newlines.sh && echo "✔ EOF newlines ok" || exit 1
 ./ci/test_tabs.sh && echo "✔ indentation ok" || exit 1
 ./ci/test_trailing_whitespace.sh && echo "✔ trailing whitespace ok" || exit 1
 ./ci/test_notebooks_clear.sh && echo "✔ notebooks ok" || exit 1
