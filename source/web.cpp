770738ec0593831fbb675263ecd70dcaa98aad26
mmore500 <mmore500@users.noreply.github.com>
2021-10-03 23:12:21 +0000

    Deploying to binder from @ mmore500/dishtiny@4e6873e4de1bb5d0748f99f765091fbb8ce91db1 🚀

--- source/web.cpp
+++ source/web.cpp
@@ -1,36 +1,41 @@
 #define DISH2_LOG_ENABLE
 
-#include <emscripten.h>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
 #include "conduit/include/uitsl/polyfill/ompi_mpi_comm_world.hpp"
 #include "Empirical/include/emp/config/ArgManager.hpp"
+#include "Empirical/include/emp/web/init.hpp"
 #include "Empirical/include/emp/web/UrlParams.hpp"
 
 #include "dish2/config/cfg.hpp"
 #include "dish2/config/make_arg_specs.hpp"
 #include "dish2/config/setup.hpp"
-#include "dish2/config/thread_idx.hpp"
+#include "dish2/config/TemporaryThreadIdxOverride.hpp"
 #include "dish2/spec/print_spec.hpp"
 #include "dish2/spec/Spec.hpp"
+#include "dish2/utility/print_js_stacktrace.hpp"
 #include "dish2/web/WebInterface.hpp"
 #include "dish2/world/ProcWorld.hpp"
 
 using Spec = DISH2_SPEC;
 
+// permanent in this case
+dish2::TemporaryThreadIdxOverride override{ 0 };
+
+// intentionally leaked
 thread_local dish2::WebInterface<Spec>* interface;
 
-int main() {
+void do_main() {
 
-  dish2::thread_idx = 0;
-
-  dish2::setup( emp::ArgManager{
+  dish2::setup<Spec>( emp::ArgManager{
     emp::web::GetUrlParams(), dish2::make_arg_specs<Spec>()
   } );
   dish2::print_spec<Spec>();
 
-  interface = new dish2::WebInterface<Spec>;
-
   // set up web interface
+  interface = new dish2::WebInterface<Spec>;
   interface->Redraw();
 
   emscripten_set_main_loop([](){}, 1, true);
@@ -38,5 +43,23 @@ int main() {
   // once we're done setting up, turn off the loading modal
   // MAIN_THREAD_EM_ASM({ $('.modal').modal('hide'); });
 
+}
+
+int main() {
+
+  emp::Initialize();
+
+  // called when exception handling fails
+  std::set_terminate( dish2::print_js_stacktrace );
+
+  try {
+    do_main();
+  } catch(const std::exception &e) {
+    std::cout << "Uncaught exception: " << e.what() << '\n';
+  } catch(...) {
+    std::cout << "Uncaught exception: unknown" << '\n';
+  }
+
   return 0;
+
 }
