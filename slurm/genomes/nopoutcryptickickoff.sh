9f4f58c0d73719110eda8aaa70ecccaee7b39011
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-09-22 16:59:33 -0400

    Fix genome url

--- slurm/genomes/nopoutcryptickickoff.sh
+++ slurm/genomes/nopoutcryptickickoff.sh
@@ -105,7 +105,7 @@ container_tag: "${CONTAINER_TAG}"
 repo_sha: "${REPO_SHA}"
 series: "\${just_one_series}"
 stint: "${STINT}"
-target_genome_url: "s3://${BUCKET}/endeavor=\$(( just_one_series / 1000 ))/genomes/stage=0+what=generated/stint=${STINT}/series=\${just_one_series}/a=genome+criteria=abundance+morph=jenga_phenotype_equivalent_nopout+proc=0+series=\${just_one_series}+stint=${STINT}+thread=0+variation=master+ext=.json.gz"
+target_genome_url: "s3://${BUCKET}/endeavor=\$(( just_one_series / 1000 ))/genomes/stage=1+what=generated/stint=${STINT}/series=\${just_one_series}/a=genome+criteria=abundance+morph=jenga_phenotype_equivalent_nopout+proc=0+series=\${just_one_series}+stint=${STINT}+thread=0+variation=master+ext=.json.gz"
 followup_freq: "${FOLLOWUP_FREQ}"
 J2_HEREDOC_EOF
 
