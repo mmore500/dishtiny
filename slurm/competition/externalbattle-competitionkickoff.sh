3a9b2f226067976f87d48cd646a8f07b6df71f72
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-09-22 14:38:08 -0400

    Fix extension

--- slurm/competition/externalbattle-competitionkickoff.sh
+++ slurm/competition/externalbattle-competitionkickoff.sh
@@ -150,8 +150,8 @@ bucket: ${BUCKET}
 configpack: ${CONFIGPACK}
 container_tag: ${CONTAINER_TAG}
 repo_sha: ${REPO_SHA}
-first_competitor_url: "s3://${BUCKET}/endeavor=\${ENDEAVOR}/populations/stage=0+what=generated/stint=\${STINT}/series=${SERIES}/a=population+proc=0+series=${SERIES}+stint=\${STINT}+thread=0+variation=master+ext=.json.gz"
-second_competitor_url: "s3://${BUCKET}/endeavor=\${ENDEAVOR}/populations/stage=0+what=generated/stint=\${STINT}/series=\${comparator_series}/a=population+proc=0+series=\${comparator_series}+stint=\${STINT}+thread=0+variation=master+ext=.json.gz"
+first_competitor_url: "s3://${BUCKET}/endeavor=\${ENDEAVOR}/populations/stage=0+what=generated/stint=\${STINT}/series=${SERIES}/a=population+proc=0+series=${SERIES}+stint=\${STINT}+thread=0+variation=master+ext=.bin.xz"
+second_competitor_url: "s3://${BUCKET}/endeavor=\${ENDEAVOR}/populations/stage=0+what=generated/stint=\${STINT}/series=\${comparator_series}/a=population+proc=0+series=\${comparator_series}+stint=\${STINT}+thread=0+variation=master+ext=.bin.xz"
 output_url: "s3://${BUCKET}/endeavor=\${ENDEAVOR}/external-battles/stage=1+what=generated/stint=\${STINT}/series=${SERIES}/"
 replicate: \${replicate}
 endeavor: \${ENDEAVOR}
