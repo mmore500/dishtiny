7ff1793fbe53e8e1368134db82e0413f2becb4b4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-09-23 18:48:40 -0400

    Try switching to curl

--- script/repro_runner.sh
+++ script/repro_runner.sh
@@ -514,7 +514,7 @@ if [ -n "${repo_sha}" ]; then
   [[ -f ~/.secrets.sh ]] && source ~/.secrets.sh
   gitget_path="$(mktemp)"
   echo "gitget_path ${gitget_path}"
-  wget -O "${gitget_path}" "https://raw.githubusercontent.com/mmore500/dishtiny/${repo_sha}/script/gitget.sh"
+  curl --fail --show-error --silent --location --retry 10 --retry-delay 1 --max-time 15 --connect-timeout 20 "https://raw.githubusercontent.com/mmore500/dishtiny/${repo_sha}/script/gitget.sh" -o "${gitget_path}"
   echo "wget success"
   chmod +x "${gitget_path}"
   echo "${gitget_path} ready"
