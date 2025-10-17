d7625814bba52d686fc4bdf02c1f6517d21c7e62
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-16 21:19:27 -0400

    Update mail-user

--- microbenchmarks/slurm/nnode=1+nproc=8+ext=.slurm.sh
+++ microbenchmarks/slurm/nnode=1+nproc=8+ext=.slurm.sh
@@ -8,6 +8,7 @@
 #SBATCH --account=devolab
 #SBATCH --output="/mnt/scratch/%u/slurmlogs/slurm-%A.out"
 #SBATCH --mail-type=FAIL
+#SBATCH --mail-user=mawni4ah2o@pomail.net
 # No --mail-user, the default value is the submitting user
 
 export CONTEXT=icer
